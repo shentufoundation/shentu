78183fa62a655690253b8aabae331fc1416a880d
Jun <junhong.chen@certik.org>
2020-09-12 14:55:07 -0400

    Refactor.

--- toolsets/oracle-operator/http.go
+++ toolsets/oracle-operator/http.go
@@ -1,4 +1,4 @@
-package querier
+package oracle
 
 import (
 	"bytes"
@@ -13,16 +13,16 @@ import (
 	"github.com/certikfoundation/shentu/toolsets/oracle-operator/types"
 )
 
-// packRequest packs relayer ServiceRequest to http request.
-func packRequest(ctx types.Context, msgCreateTask types.MsgCreateTask, endpoint string) (*http.Request, error) {
+// packRequest packs payload to http request.
+func packRequest(ctx types.Context, endpoint string, payload interface{}) (*http.Request, error) {
 	var err error
 	var req *http.Request
-	logger := ctx.Logger().With("type", "msgCreateTask")
+	logger := ctx.Logger().With("type", "request", "endpiont", endpoint, "payload", payload)
 	config := ctx.Config()
 
 	var requestBody []byte
 
-	if strings.EqualFold(config.Querier.Method, "GET") {
+	if strings.EqualFold(config.Method, "GET") {
 		u, err := url.Parse(endpoint)
 		if err != nil {
 			logger.Error(err.Error())
@@ -30,15 +30,15 @@ func packRequest(ctx types.Context, msgCreateTask types.MsgCreateTask, endpoint 
 		}
 		endpoint = u.String()
 		logger.Debug("query endpoint with GET method", "endpoint", endpoint)
-	} else if strings.EqualFold(config.Querier.Method, "POST") {
-		requestBody, err = json.Marshal(msgCreateTask)
+	} else if strings.EqualFold(config.Method, "POST") {
+		requestBody, err = json.Marshal(payload)
 		if err != nil {
 			logger.Error(err.Error())
 			return nil, err
 		}
 	}
 
-	req, err = http.NewRequest(strings.ToUpper(config.Querier.Method), endpoint, bytes.NewBuffer(requestBody))
+	req, err = http.NewRequest(strings.ToUpper(config.Method), endpoint, bytes.NewBuffer(requestBody))
 	if err != nil {
 		logger.Error(err.Error())
 		return nil, err
@@ -46,7 +46,7 @@ func packRequest(ctx types.Context, msgCreateTask types.MsgCreateTask, endpoint 
 	return req, err
 }
 
-// packResponse packs http response with previous passed relayer ServiceRequest to ServiceResponse.
+// packResponse packs http response to single score.
 func packResponse(ctx types.Context, response *http.Response) (uint8, error) {
 	logger := ctx.Logger().With("type", "response")
 	logger.Debug("to pack response", "data", response.Body)
@@ -57,40 +57,38 @@ func packResponse(ctx types.Context, response *http.Response) (uint8, error) {
 		return 0, err
 	}
 
-	var sResData uint8
-	err = json.Unmarshal(body, &sResData)
+	var result uint8
+	err = json.Unmarshal(body, &result)
 	if err != nil {
 		logger.Error("unmarshal response data", "error", err.Error(), "body", body)
 		return 0, err
 	}
-	return sResData, nil
+	return result, nil
 }
 
-// HandleRequest provides interface for relayer to call service provider endpoint.
-func HandleRequest(ctx types.Context, msgCreateTask types.MsgCreateTask, endpoint string) (uint8, error) {
-	// TODO: revise to async call
+// handleRequest queries primitive endpoint.
+func handleRequest(ctx types.Context, endpoint string, payload interface{}) (uint8, error) {
 	var response *http.Response
-	var RETRY = ctx.Config().Querier.RetryTimes
-
-	// pack ServiceRequest to be an http request
-	req, err := packRequest(ctx, msgCreateTask, endpoint)
+	var RETRY = ctx.Config().RetryTimes
+	var TIMEOUT = ctx.Config().Timeout
+	// pack task to be an http request
+	req, err := packRequest(ctx, endpoint, payload)
 	if err != nil {
-		ctx.Logger().Error("pack request", "error", err.Error())
+		ctx.Logger().Error("packing request", "error", err.Error())
 		return 0, err
 	}
 	client := &http.Client{
-		Timeout: time.Duration(ctx.Config().Querier.Timeout) * time.Second,
+		Timeout: time.Duration(TIMEOUT) * time.Second,
 	}
-
-	// RETRY is 3 by default
+	// make http query
 	for i := 0; i < RETRY; i++ {
 		response, err = client.Do(req)
 		if response == nil {
-			ctx.Logger().Info("Response empty")
+			ctx.Logger().Debug("response empty")
 			continue
 		}
 		if response.Body == nil {
-			ctx.Logger().Info("Response Body empty")
+			ctx.Logger().Debug("response Body empty")
 			continue
 		}
 		if err == nil {
@@ -99,22 +97,22 @@ func HandleRequest(ctx types.Context, msgCreateTask types.MsgCreateTask, endpoin
 		}
 		defer response.Body.Close()
 
-		ctx.Logger().Info("Retrying requests", "retries", i, "reason", err.Error())
+		ctx.Logger().Debug("retrying requests", "retries", i, "reason", err.Error())
 	}
 	if response == nil {
-		ctx.Logger().Info("Response empty")
+		ctx.Logger().Debug("response empty")
 		return 0, fmt.Errorf("response empty")
 	}
 	if response.StatusCode != 200 {
-		ctx.Logger().Info("Request not success", "status", response.Status)
+		ctx.Logger().Debug("request not success", "status", response.Status)
 		return 0, fmt.Errorf("request not success")
 	}
-
-	sres, err := packResponse(ctx, response)
+	// extract result from http response
+	result, err := packResponse(ctx, response)
 	if err != nil {
 		ctx.Logger().Error("pack response", "error", err.Error())
 		return 0, err
 	}
-	ctx.Logger().Debug("Query Security Score from endpoint", "score", sres)
-	return sres, nil
+	ctx.Logger().Debug("result from endpoint", "result", result)
+	return result, nil
 }
