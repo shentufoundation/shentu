27f9385496c511d354abc5bc4f060848da21f77f
hacheigriega <hykim.home@gmail.com>
2020-09-16 02:58:22 +0900

    a bit more unit testing for manual vesting acc

--- x/auth/vesting/vesting_account_test.go
+++ x/auth/vesting/vesting_account_test.go
@@ -487,12 +487,13 @@ func TestTriggeredVestingAccountJSON(t *testing.T) {
 func TestManualVestingAcc(t *testing.T) {
 	// Account setup
 	origCoins := sdk.Coins{sdk.NewInt64Coin(denom, 1000)}
-	bacc := authtypes.NewBaseAccount(addr, origCoins, nil, 0, 0)
-	baseVestingAccount, err := authvesting.NewBaseVestingAccount(bacc, origCoins, 0)
+	ba := authtypes.NewBaseAccount(addr, origCoins, nil, 0, 0)
+	bva, err := authvesting.NewBaseVestingAccount(ba, origCoins, 0)
 	require.Nil(t, err)
 
-	mva := NewManualVestingAccountRaw(baseVestingAccount, sdk.NewCoins())
+	mva := NewManualVestingAccountRaw(bva, sdk.NewCoins())
 
+	// Test GetVestedCoins and GetVestingCoins
 	now := tmtime.Now()
 	vestedCoins := mva.GetVestedCoins(now)
 	require.Nil(t, vestedCoins)
@@ -506,4 +507,39 @@ func TestManualVestingAcc(t *testing.T) {
 	require.Equal(t, sdk.Coins{sdk.NewInt64Coin(denom, 700)}, vestedCoins)
 	vestingCoins = mva.GetVestingCoins(now)
 	require.Equal(t, sdk.Coins{sdk.NewInt64Coin(denom, 300)}, vestingCoins)
+
+	// Test JSON (un)marshal
+	bz, err := json.Marshal(mva)
+	require.NoError(t, err)
+
+	bz1, err := mva.MarshalJSON()
+	require.NoError(t, err)
+	require.Equal(t, string(bz1), string(bz))
+
+	var a ManualVestingAccount
+	require.NoError(t, json.Unmarshal(bz, &a))
+	require.Equal(t, mva.String(), a.String())
+
+
+	// New account setup
+	origCoins = sdk.Coins{sdk.NewInt64Coin(denom, 1000)}
+	origVesting := sdk.Coins{sdk.NewInt64Coin(denom, 300)}
+
+	ba2 := authtypes.NewBaseAccount(addr, origCoins, nil, 0, 0)
+	bva2, err := authvesting.NewBaseVestingAccount(ba2, origVesting, 0)
+	require.Nil(t, err)
+	mva2 := NewManualVestingAccountRaw(bva2, sdk.NewCoins())
+
+	// Test SpendableCoins
+	spendableCoins := mva2.SpendableCoins(now)
+	require.Equal(t, sdk.Coins{sdk.NewInt64Coin(denom, 700)}, spendableCoins)
+	
+	coinToUnlock = sdk.NewCoin(denom, sdk.NewInt(150))
+	mva2.VestedCoins = mva2.VestedCoins.Add(coinToUnlock)
+
+	spendableCoins = mva2.SpendableCoins(now)
+	require.Equal(t, sdk.Coins{sdk.NewInt64Coin(denom, 850)}, spendableCoins)
+
+	// TODO: Test delegation, undelegation, genesis validation
+
 }
