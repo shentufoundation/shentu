4acaaf737fced5a5a4b29c439de9e00975f8b1be
hacheigriega <hykim.home@gmail.com>
2020-09-17 08:38:20 +0900

    forgot to import

--- x/auth/client/cli/tx.go
+++ x/auth/client/cli/tx.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/spf13/cobra"
 
+	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/context"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/codec"
