c0a722d80a54e359896f97fab8a66166710d5dbc
Ziyi Zhang <66639536+ziyi-zhang-1130@users.noreply.github.com>
2020-10-08 12:56:04 -0400

    simulation: change sim denom back to `stake` (#30)
    * change sim denom back to `stake`
    
    * fix mint module's sim methods
    
    Co-authored-by: yoongbok-lee <52583590+yoongbok-lee@users.noreply.github.com>

--- x/staking/module.go
+++ x/staking/module.go
@@ -173,7 +173,7 @@ func (am AppModule) EndBlock(ctx sdk.Context, rbb abci.RequestEndBlock) []abci.V
 
 // GenerateGenesisState creates a randomized GenState of this module.
 func (AppModuleBasic) GenerateGenesisState(simState *module.SimulationState) {
-	simulation.RandomizedGenState(simState)
+	stakingSim.RandomizedGenState(simState)
 }
 
 // RegisterStoreDecoder registers a decoder for staking module.
