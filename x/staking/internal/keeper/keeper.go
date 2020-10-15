2c002e0dc0bf9a0530681f78baaa92fed2dd22d2
yoongbok lee <yoongbok.lee@certik.org>
2020-10-15 17:16:08 -0400

    add 100% commission rate

--- x/staking/internal/keeper/keeper.go
+++ x/staking/internal/keeper/keeper.go
@@ -3,6 +3,8 @@ package keeper
 import (
 	"time"
 
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/params"
@@ -24,4 +26,25 @@ func NewKeeper(cdc *codec.Codec, key sdk.StoreKey, supplyKeeper types.SupplyKeep
 
 func (k Keeper) RemoveUBDQueue(ctx sdk.Context, timestamp time.Time) {
 	ctx.KVStore(k.storeKey).Delete(staking.GetUnbondingDelegationTimeKey(timestamp))
+}
+
+// UpdateValidatorCommission attempts to update a validator's commission rate.
+// An error is returned if the new commission rate is invalid.
+func (k Keeper) UpdateValidatorCommission(ctx sdk.Context,
+	validator types.Validator, newRate sdk.Dec) (types.Commission, error) {
+	if newRate.LT(sdk.OneDec()) {
+		return types.Commission{}, sdkerrors.Register(types.ModuleName, 100, "commission cannot be less than 100%")
+	}
+
+	commission := validator.Commission
+	blockTime := ctx.BlockHeader().Time
+
+	if err := commission.ValidateNewRate(newRate, blockTime); err != nil {
+		return commission, err
+	}
+
+	commission.Rate = newRate
+	commission.UpdateTime = blockTime
+
+	return commission, nil
 }
