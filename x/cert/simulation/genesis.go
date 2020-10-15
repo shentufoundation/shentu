c5fde450282b340cbeac1572808e2440ec2910da
ziyi-zhang-1130 <ziyi.zhang@certik.org>
2020-10-15 14:28:48 -0400

    change the way to choose certfiers in genesis

--- x/cert/simulation/genesis.go
+++ x/cert/simulation/genesis.go
@@ -2,21 +2,19 @@ package simulation
 
 import (
 	"github.com/cosmos/cosmos-sdk/types/module"
-	"github.com/cosmos/cosmos-sdk/x/simulation"
 
 	"github.com/certikfoundation/shentu/x/cert/internal/types"
 )
 
 // RandomizedGenState creates a random genesis state for module simulation.
 func RandomizedGenState(simState *module.SimulationState) {
-	r := simState.Rand
 	gs := types.GenesisState{}
 
-	numOfCertifiers := r.Intn(10)
-	for i := 0; i < numOfCertifiers; i++ {
-		acc, _ := simulation.RandomAcc(r, simState.Accounts)
-		certifier := types.NewCertifier(acc.Address, "", nil, "")
-		gs.Certifiers = append(gs.Certifiers, certifier)
+	for _, acc := range simState.Accounts {
+		if simState.Rand.Intn(100) < 10 {
+			certifier := types.NewCertifier(acc.Address, "", nil, "")
+			gs.Certifiers = append(gs.Certifiers, certifier)
+		}
 	}
 
 	simState.GenState[types.ModuleName] = simState.Cdc.MustMarshalJSON(gs)
