dde242d3754ae579b397138ae8041ff377701dc3
yoongbok lee <yoongbok.lee@certik.org>
2020-10-22 14:33:21 -0400

    add in some more invs

--- x/shield/keeper/invariants.go
+++ x/shield/keeper/invariants.go
@@ -11,10 +11,11 @@ import (
 // RegisterInvariants registers all shield invariants.
 func RegisterInvariants(ir sdk.InvariantRegistry, k Keeper) {
 	ir.RegisterRoute(types.ModuleName, "module-account", ModuleAccountInvariant(k))
+	ir.RegisterRoute(types.ModuleName, "withdraw", ProviderInvariant(k))
 }
 
 // ModuleAccountInvariant checks that the module account coins reflects the sum of
-// deposit amounts held on store
+// remaining services and rewards held on store
 func ModuleAccountInvariant(keeper Keeper) sdk.Invariant {
 	return func(ctx sdk.Context) (string, bool) {
 		total := keeper.GetRemainingServiceFees(ctx)
@@ -33,5 +34,51 @@ func ModuleAccountInvariant(keeper Keeper) sdk.Invariant {
 				"\n\tsum of remaining service fees & rewards amount:  %s"+
 				"\n\tremaining change amount: %s\n",
 				moduleCoins, totalInt, change)), broken
+	}
+}
+
+// ProviderInvariant checks that the providers' coin amounts equal to the tracked value.
+func ProviderInvariant(keeper Keeper) sdk.Invariant {
+	return func(ctx sdk.Context) (string, bool) {
+		providers := keeper.GetAllProviders(ctx)
+		withdrawSum := sdk.NewInt(0)
+		collateralSum := sdk.NewInt(0)
+		for _, prov := range providers {
+			withdrawSum = withdrawSum.Add(prov.Withdrawing)
+			collateralSum = collateralSum.Add(prov.Collateral)
+		}
+
+		totalWithdraw := keeper.GetTotalWithdrawing(ctx)
+		totalCollateral := keeper.GetTotalCollateral(ctx)
+		broken := !totalWithdraw.Equal(withdrawSum) || !totalCollateral.Equal(collateralSum)
+
+		return sdk.FormatInvariant(types.ModuleName, "module-account",
+			fmt.Sprintf("\n\ttotal withdraw amount: %s"+
+				"\n\tsum of providers' withdrawing amount:  %s"+
+				"\n\ttotal collateral amount: %s"+
+				"\n\tsum of providers' collateral amount: %s\bn",
+				totalWithdraw, withdrawSum, totalCollateral, collateralSum)), broken
+	}
+}
+
+// ShieldInvariant checks that the providers' coin amounts equal to the tracked value.
+func ShieldInvariant(keeper Keeper) sdk.Invariant {
+	return func(ctx sdk.Context) (string, bool) {
+		pools := keeper.GetAllPools(ctx)
+		shieldSum := sdk.NewInt(0)
+		for _, pool := range pools {
+			shieldSum = shieldSum.Add(pool.Shield)
+		}
+
+		totalWithdraw := keeper.GetTotalWithdrawing(ctx)
+		totalCollateral := keeper.GetTotalCollateral(ctx)
+		broken := !totalWithdraw.Equal(shieldSum)
+
+		return sdk.FormatInvariant(types.ModuleName, "module-account",
+			fmt.Sprintf("\n\ttotal withdraw amount: %s"+
+				"\n\tsum of providers' withdrawing amount:  %s"+
+				"\n\ttotal collateral amount: %s"+
+				"\n\tsum of providers' collateral amount: %s\bn",
+				totalWithdraw, withdrawSum, totalCollateral, collateralSum)), broken
 	}
 }
