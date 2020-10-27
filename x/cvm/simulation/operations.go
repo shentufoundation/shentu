1d371f0194f5d2fd937e3b11e55a57c29f9205f0
ziyi-zhang-1130 <ziyi.zhang@certik.org>
2020-10-27 16:28:01 -0400

    modify hello55 deploy tx

--- x/cvm/simulation/operations.go
+++ x/cvm/simulation/operations.go
@@ -2,6 +2,7 @@ package simulation
 
 import (
 	"encoding/hex"
+	"fmt"
 	"math/rand"
 
 	"github.com/cosmos/cosmos-sdk/baseapp"
@@ -11,25 +12,16 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/simulation"
 
-	"github.com/hyperledger/burrow/txs/payload"
-
 	"github.com/certikfoundation/shentu/x/cvm/internal/keeper"
 	"github.com/certikfoundation/shentu/x/cvm/internal/types"
 )
 
 const (
-	OpWeightMsgCall   = "op_weight_msg_call"
 	OpWeightMsgDeploy = "op_weight_msg_deploy"
 )
 
 // WeightedOperations creates an operation with a weight for each type of message generators.
 func WeightedOperations(appParams simulation.AppParams, cdc *codec.Codec, k keeper.Keeper) simulation.WeightedOperations {
-	var weightMsgCall int
-	appParams.GetOrGenerate(cdc, OpWeightMsgCall, &weightMsgCall, nil,
-		func(_ *rand.Rand) {
-			weightMsgCall = simappparams.DefaultWeightMsgSend
-		})
-
 	var weightMsgDeploy int
 	appParams.GetOrGenerate(cdc, OpWeightMsgDeploy, &weightMsgDeploy, nil,
 		func(_ *rand.Rand) {
@@ -37,8 +29,7 @@ func WeightedOperations(appParams simulation.AppParams, cdc *codec.Codec, k keep
 		})
 
 	return simulation.WeightedOperations{
-		simulation.NewWeightedOperation(weightMsgCall, SimulateMsgCall(k)),
-		simulation.NewWeightedOperation(weightMsgDeploy, SimulateMsgDeploy(k)),
+		simulation.NewWeightedOperation(weightMsgDeploy, SimulateMsgDeployHello55(k)),
 	}
 }
 
@@ -77,17 +68,17 @@ func SimulateMsgCall(k keeper.Keeper) simulation.Operation {
 	}
 }
 
-// SimulateMsgDeploy creates a massage operation of MsgDeploy with randomized field values.
-func SimulateMsgDeploy(k keeper.Keeper) simulation.Operation {
+// SimulateMsgDeployHello55 creates a massage operation of MsgDeploy with randomized field values.
+func SimulateMsgDeployHello55(k keeper.Keeper) simulation.Operation {
 	return func(r *rand.Rand, app *baseapp.BaseApp, ctx sdk.Context, accs []simulation.Account, chainID string) (
 		simulation.OperationMsg, []simulation.FutureOperation, error) {
 		caller, _ := simulation.RandomAcc(r, accs)
-		value := uint64(0)
-		code, _ := hex.DecodeString(keeper.Hello55BytecodeString)
-		abi := keeper.Hello55AbiJsonString
-		var meta []*payload.ContractMeta
+		code, err := hex.DecodeString(Hello55Code)
+		if err != nil {
+			return simulation.NoOpMsg(types.ModuleName), nil, err
+		}
 
-		msg := types.NewMsgDeploy(caller.Address, value, code, abi, meta, false, false)
+		msg := types.NewMsgDeploy(caller.Address, uint64(0), code, Hello55Abi, nil, false, false)
 
 		account := k.AuthKeeper().GetAccount(ctx, caller.Address)
 		fees, err := simulation.RandomFees(r, ctx, account.SpendableCoins(ctx.BlockTime()))
@@ -105,10 +96,18 @@ func SimulateMsgDeploy(k keeper.Keeper) simulation.Operation {
 			caller.PrivKey,
 		)
 
-		_, _, err = app.Deliver(tx)
+		_, res, err := app.Deliver(tx)
 		if err != nil {
 			return simulation.NoOpMsg(types.ModuleName), nil, err
 		}
+
+		contractAddr, err := sdk.AccAddressFromBech32(string(res.Events.ToABCIEvents()[2].Attributes[0].Value))
+		if err != nil {
+			return simulation.NoOpMsg(types.ModuleName), nil, nil
+		}
+
+		fmt.Printf("<<<<<<<<<<<<<< Contract addr: %s\n", contractAddr)
+
 		return simulation.NewOperationMsg(msg, true, ""), nil, nil
 	}
 }
