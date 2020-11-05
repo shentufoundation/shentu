f3ed0e0b8d4d70e1df99ef754e780ef629139813
Dan <57596823+dshe-certik@users.noreply.github.com>
2020-11-05 15:37:13 -0500

    gov: fix parameter change proposals (#145)
    * Increased voters and yes votes in simulations.
    
    * remove voting param change from gov proposal
    
    * correct tally params in gov param-change sim
    
    * fix tally params in gov param-change sim
    
    Co-authored-by: ziyi-zhang-1130 <ziyi.zhang@certik.org>

--- x/gov/simulation/params.go
+++ x/gov/simulation/params.go
@@ -6,7 +6,6 @@ import (
 	"math/rand"
 	"time"
 
-	sdk "github.com/cosmos/cosmos-sdk/types"
 	govTypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 	"github.com/cosmos/cosmos-sdk/x/simulation"
 )
@@ -15,9 +14,6 @@ const (
 	keyVotingParams  = "votingparams"
 	keyDepositParams = "depositparams"
 	keyTallyParams   = "tallyparams"
-	subkeyQuorum     = "quorum"
-	subkeyThreshold  = "threshold"
-	subkeyVeto       = "veto"
 )
 
 // ParamChanges defines the parameters that can be modified by param change proposals
@@ -25,7 +21,6 @@ const (
 func ParamChanges(r *rand.Rand) []simulation.ParamChange {
 	votingPeriod := time.Duration(simulation.RandIntBetween(r, 1, 2*60*60*24*2)) * time.Second
 	depositPeriod := time.Duration(simulation.RandIntBetween(r, 1, 2*60*60*24*2)) * time.Second
-	tallyParams := GenerateATallyParams(r)
 
 	return []simulation.ParamChange{
 		simulation.NewSimParamChange(govTypes.ModuleName, keyVotingParams,
@@ -40,30 +35,7 @@ func ParamChanges(r *rand.Rand) []simulation.ParamChange {
 		),
 		simulation.NewSimParamChange(govTypes.ModuleName, keyTallyParams,
 			func(r *rand.Rand) string {
-				changes := []struct {
-					key   string
-					value sdk.Dec
-				}{
-					{subkeyQuorum, tallyParams.Quorum},
-					{subkeyThreshold, tallyParams.Threshold},
-					{subkeyVeto, tallyParams.Veto},
-				}
-
-				pc := make(map[string]string)
-				numChanges := simulation.RandIntBetween(r, 1, len(changes))
-				for i := 0; i < numChanges; i++ {
-					c := changes[r.Intn(len(changes))]
-
-					_, ok := pc[c.key]
-					for ok {
-						c := changes[r.Intn(len(changes))]
-						_, ok = pc[c.key]
-					}
-
-					pc[c.key] = c.value.String()
-				}
-
-				bz, _ := json.Marshal(pc)
+				bz, _ := json.Marshal(GenerateTallyParams(r))
 				return string(bz)
 			},
 		),
