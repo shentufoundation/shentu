668640685d3abaf8fc8e5ea7505ac6b3c5036274
Dan <dan.she@certik.io>
2020-11-05 14:27:09 -0500

    Re-enabled param change sims in gov. To be fixed in another PR.

--- x/gov/simulation/params.go
+++ x/gov/simulation/params.go
@@ -1,12 +1,12 @@
 package simulation
 
 import (
-	// "encoding/json"
+	"encoding/json"
 	"fmt"
 	"math/rand"
 	"time"
 
-	// sdk "github.com/cosmos/cosmos-sdk/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	govTypes "github.com/cosmos/cosmos-sdk/x/gov/types"
 	"github.com/cosmos/cosmos-sdk/x/simulation"
 )
@@ -25,7 +25,7 @@ const (
 func ParamChanges(r *rand.Rand) []simulation.ParamChange {
 	votingPeriod := time.Duration(simulation.RandIntBetween(r, 1, 2*60*60*24*2)) * time.Second
 	depositPeriod := time.Duration(simulation.RandIntBetween(r, 1, 2*60*60*24*2)) * time.Second
-	// tallyParams := GenerateATallyParams(r)
+	tallyParams := GenerateATallyParams(r)
 
 	return []simulation.ParamChange{
 		simulation.NewSimParamChange(govTypes.ModuleName, keyVotingParams,
@@ -38,36 +38,34 @@ func ParamChanges(r *rand.Rand) []simulation.ParamChange {
 				return fmt.Sprintf(`{"max_deposit_period": "%d"}`, depositPeriod)
 			},
 		),
-		/*
-			simulation.NewSimParamChange(govTypes.ModuleName, keyTallyParams,
-				func(r *rand.Rand) string {
-					changes := []struct {
-						key   string
-						value sdk.Dec
-					}{
-						{subkeyQuorum, tallyParams.Quorum},
-						{subkeyThreshold, tallyParams.Threshold},
-						{subkeyVeto, tallyParams.Veto},
-					}
+		simulation.NewSimParamChange(govTypes.ModuleName, keyTallyParams,
+			func(r *rand.Rand) string {
+				changes := []struct {
+					key   string
+					value sdk.Dec
+				}{
+					{subkeyQuorum, tallyParams.Quorum},
+					{subkeyThreshold, tallyParams.Threshold},
+					{subkeyVeto, tallyParams.Veto},
+				}
 
-					pc := make(map[string]string)
-					numChanges := simulation.RandIntBetween(r, 1, len(changes))
-					for i := 0; i < numChanges; i++ {
+				pc := make(map[string]string)
+				numChanges := simulation.RandIntBetween(r, 1, len(changes))
+				for i := 0; i < numChanges; i++ {
+					c := changes[r.Intn(len(changes))]
+
+					_, ok := pc[c.key]
+					for ok {
 						c := changes[r.Intn(len(changes))]
-
-						_, ok := pc[c.key]
-						for ok {
-							c := changes[r.Intn(len(changes))]
-							_, ok = pc[c.key]
-						}
-
-						pc[c.key] = c.value.String()
+						_, ok = pc[c.key]
 					}
 
-					bz, _ := json.Marshal(pc)
-					return string(bz)
-				},
-			),
-		*/
+					pc[c.key] = c.value.String()
+				}
+
+				bz, _ := json.Marshal(pc)
+				return string(bz)
+			},
+		),
 	}
 }
