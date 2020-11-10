be7ab7f3dde5e2581cb625e3efe2311a96833311
Zach Page <zach.page@certik.org>
2020-11-10 11:12:30 -0500

    Remove unused declarations

--- x/gov/client/utils/utils.go
+++ x/gov/client/utils/utils.go
@@ -5,7 +5,6 @@ import (
 	"strings"
 
 	govTypes "github.com/cosmos/cosmos-sdk/x/gov/types"
-	"github.com/cosmos/cosmos-sdk/x/upgrade"
 )
 
 // NormalizeVoteOption normalizes user specified vote option
@@ -22,36 +21,4 @@ func NormalizeVoteOption(option string) string {
 	default:
 		return option
 	}
-}
-
-// NormalizeProposalType normalizes user specified proposal type.
-func NormalizeProposalType(proposalType string) string {
-	switch strings.ToLower(proposalType) {
-	case "text":
-		return govTypes.ProposalTypeText
-	case "softwareupgrade":
-		return upgrade.ProposalTypeSoftwareUpgrade
-	default:
-		return proposalType
-	}
-}
-
-// NormalizeProposalStatus normalizes user specified proposal status.
-func NormalizeProposalStatus(status string) string {
-	status = strings.ToUpper(status)
-	switch status {
-	case "DEPOSITPERIOD", "DEPOSIT_PERIOD":
-		return "DepositPeriod"
-	case "VOTINGPERIOD", "VOTING_PERIOD":
-		return "VotingPeriod"
-	case "PASSED":
-		return "Passed"
-	case "REJECTED":
-		return "Rejected"
-	case "FAILED":
-		return "Failed"
-	case "SECONDVOTINGPERIOD", "SECOND_VOTING_PERIOD":
-		return "SecondVotingPeriod"
-	}
-	return ""
 }
