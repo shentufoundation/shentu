a755ea0ed3c1e9dbd2476153ec36bc3d96615c67
Hyoung-yoon Kim <hykim.home@gmail.com>
2020-11-30 23:52:19 +0900

    testing: Use simapp for cvm testing and retire common tests package (#180)

--- x/cvm/internal/keeper/blockchain_test.go
+++ x/cvm/internal/keeper/blockchain_test.go
@@ -1,28 +1,35 @@
-package keeper
+package keeper_test
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/require"
+
+	abci "github.com/tendermint/tendermint/abci/types"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/certikfoundation/shentu/simapp"
+	"github.com/certikfoundation/shentu/x/cvm/internal/keeper"
 )
 
+func NewGasMeter(limit uint64) sdk.GasMeter {
+	return sdk.NewGasMeter(limit)
+}
+
 func TestNewBlockChain(t *testing.T) {
-	testInput := CreateTestInput(t)
-
-	ctx := testInput.Ctx
-	cvmk := testInput.CvmKeeper
-
-	bc := NewBlockChain(ctx, cvmk)
+	app := simapp.Setup(false)
+	ctx := app.BaseApp.NewContext(false, abci.Header{Time: time.Now().UTC()}).WithGasMeter(NewGasMeter(10000000000000))
+	bc := keeper.NewBlockChain(ctx, app.CvmKeeper)
 	require.NotNil(t, bc.LastBlockTime())
 }
 
 func TestBlockchain_BlockHash(t *testing.T) {
-	testInput := CreateTestInput(t)
+	app := simapp.Setup(false)
+	ctx := app.BaseApp.NewContext(false, abci.Header{Time: time.Now().UTC()}).WithGasMeter(NewGasMeter(10000000000000))
+	bc := keeper.NewBlockChain(ctx, app.CvmKeeper)
 
-	ctx := testInput.Ctx
-	cvmk := testInput.CvmKeeper
-
-	bc := NewBlockChain(ctx, cvmk)
 	ctxHash := ctx.BlockHeader().LastBlockId.Hash
 	bz, err := bc.BlockHash(0)
 	require.Nil(t, err)
@@ -34,22 +41,18 @@ func TestBlockchain_BlockHash(t *testing.T) {
 }
 
 func TestBlockchain_LastBlockHeight(t *testing.T) {
-	testInput := CreateTestInput(t)
-
-	ctx := testInput.Ctx
-	cvmk := testInput.CvmKeeper
-	bc := NewBlockChain(ctx, cvmk)
+	app := simapp.Setup(false)
+	ctx := app.BaseApp.NewContext(false, abci.Header{Time: time.Now().UTC()}).WithGasMeter(NewGasMeter(10000000000000))
+	bc := keeper.NewBlockChain(ctx, app.CvmKeeper)
 
 	ctxHeight := ctx.BlockHeader().Height
 	require.Equal(t, uint64(ctxHeight), bc.LastBlockHeight())
 }
 
 func TestBlockchain_LastBlockTime(t *testing.T) {
-	testInput := CreateTestInput(t)
-
-	ctx := testInput.Ctx
-	cvmk := testInput.CvmKeeper
-	bc := NewBlockChain(ctx, cvmk)
+	app := simapp.Setup(false)
+	ctx := app.BaseApp.NewContext(false, abci.Header{Time: time.Now().UTC()}).WithGasMeter(NewGasMeter(10000000000000))
+	bc := keeper.NewBlockChain(ctx, app.CvmKeeper)
 
 	ctxTime := ctx.BlockHeader().Time
 	require.Equal(t, ctxTime, bc.LastBlockTime())
