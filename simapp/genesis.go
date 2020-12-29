a9303c54e309eb7b8fac82dc0a5afac39597131c
yoongbok lee <yoongbok.lee@certik.org>
2020-12-29 12:32:04 -0500

    add in simapp test helpers

---
+++ simapp/genesis.go
@@ -0,0 +1,9 @@
+package simapp
+
+import "github.com/cosmos/cosmos-sdk/simapp"
+
+// NewDefaultGenesisState generates the default state for the application.
+func NewDefaultGenesisState() simapp.GenesisState {
+	encCfg := MakeTestEncodingConfig()
+	return ModuleBasics.DefaultGenesis(encCfg.Marshaler)
+}
--- simapp/test_helpers.go
+++ simapp/test_helpers.go
@@ -1,23 +1,27 @@
 package simapp
 
 import (
+	"encoding/json"
+
+	"github.com/cosmos/cosmos-sdk/simapp"
+	banktypes "github.com/cosmos/cosmos-sdk/x/bank/types"
+
 	abci "github.com/tendermint/tendermint/abci/types"
 	"github.com/tendermint/tendermint/crypto/ed25519"
-	tmlog "github.com/tendermint/tendermint/libs/log"
+	log "github.com/tendermint/tendermint/libs/log"
 	dbm "github.com/tendermint/tm-db"
 
-	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 func Setup(isCheckTx bool) *SimApp {
 	db := dbm.NewMemDB()
-	app := NewSimApp(tmlog.NewNopLogger(), db, nil, true, map[int64]bool{}, 0)
-
+	app := NewSimApp(log.NewNopLogger(), db, nil, true, map[int64]bool{},
+		DefaultNodeHome, 5, MakeTestEncodingConfig(), simapp.EmptyAppOptions{})
 	if !isCheckTx {
 		// init chain must be called to stop deliverState from being nil
-		genesisState := ModuleBasics.DefaultGenesis()
-		stateBytes, err := codec.MarshalJSONIndent(app.Codec(), genesisState)
+		genesisState := NewDefaultGenesisState()
+		stateBytes, err := json.MarshalIndent(genesisState, "", " ")
 		if err != nil {
 			panic(err)
 		}
@@ -25,11 +29,13 @@ func Setup(isCheckTx bool) *SimApp {
 		// Initialize the chain
 		app.InitChain(
 			abci.RequestInitChain{
-				Validators:    []abci.ValidatorUpdate{},
-				AppStateBytes: stateBytes,
+				Validators:      []abci.ValidatorUpdate{},
+				ConsensusParams: simapp.DefaultConsensusParams,
+				AppStateBytes:   stateBytes,
 			},
 		)
 	}
+
 	return app
 }
 
@@ -44,26 +50,20 @@ func AddTestAddrs(app *SimApp, ctx sdk.Context, accNum int, accAmt sdk.Int) []sd
 
 	initCoins := sdk.NewCoins(sdk.NewCoin(app.StakingKeeper.BondDenom(ctx), accAmt))
 	totalSupply := sdk.NewCoins(sdk.NewCoin(app.StakingKeeper.BondDenom(ctx), accAmt.MulRaw(int64(len(testAddrs)))))
-	prevSupply := app.SupplyKeeper.GetSupply(ctx)
-	app.SupplyKeeper.SetSupply(ctx, supply.NewSupply(prevSupply.GetTotal().Add(totalSupply...)))
+	prevSupply := app.BankKeeper.GetSupply(ctx)
+	app.BankKeeper.SetSupply(ctx, banktypes.NewSupply(prevSupply.GetTotal().Add(totalSupply...)))
 
 	// fill all the addresses with some coins, set the loose pool tokens simultaneously
 	for _, addr := range testAddrs {
-		_, err := app.BankKeeper.AddCoins(ctx, addr, initCoins)
-		if err != nil {
-			panic(err)
-		}
+		_ = app.BankKeeper.AddCoins(ctx, addr, initCoins)
 	}
 	return testAddrs
 }
 
 // AddCoins adds coins to an account and increments the total supply accordingly.
 func AddCoinsToAcc(app *SimApp, ctx sdk.Context, addr sdk.AccAddress, toAdd sdk.Coins) {
-	prevSupply := app.SupplyKeeper.GetSupply(ctx)
-	app.SupplyKeeper.SetSupply(ctx, supply.NewSupply(prevSupply.GetTotal().Add(toAdd...)))
+	prevSupply := app.BankKeeper.GetSupply(ctx)
+	app.BankKeeper.SetSupply(ctx, banktypes.NewSupply(prevSupply.GetTotal().Add(toAdd...)))
 
-	_, err := app.BankKeeper.AddCoins(ctx, addr, toAdd)
-	if err != nil {
-		panic(err)
-	}
+	_ = app.BankKeeper.AddCoins(ctx, addr, toAdd)
 }
