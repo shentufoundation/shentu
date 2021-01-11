06cb43449a08a0eb8c640d7e7635c7ac51294162
Zhenhua Cao <zhenhua.cao@certik.org>
2021-01-11 15:41:50 -0500

    remove old contracts which became unrelevant today

--- toolsets/oracle-operator/contracts/ISecurityPrimitive.sol
+++
@@ -1,5 +0,0 @@
-pragma solidity ^0.6.0;
-
-interface ISecurityPrimitive {
-    function getInsight(string calldata contractAddress, string calldata functionSignature) external view returns (bool, string memory);
-}
