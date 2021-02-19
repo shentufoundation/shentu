8b873e6925bbdc746eceaa10133ce1f26748ff21
yoongbok lee <yoongbok.lee@certik.org>
2021-02-19 16:15:48 -0500

    add shield & cert migrate cmd

---
+++ cmd/certikd/cmd/migrate/cert.go
@@ -0,0 +1,376 @@
+package migrate
+
+import (
+	"encoding/hex"
+	"fmt"
+
+	certtypes "github.com/certikfoundation/shentu/x/cert/types"
+	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/tendermint/tendermint/crypto"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+// CertificateType is the type for the type of a certificate.
+type CertificateType byte
+
+const (
+	CertificateTypeNil CertificateType = iota
+	CertificateTypeCompilation
+	CertificateTypeAuditing
+	CertificateTypeProof
+	CertificateTypeOracleOperator
+	CertificateTypeShieldPoolCreator
+	CertificateTypeIdentity
+	CertificateTypeGeneral
+)
+
+// Certifier is a type for certifier.
+type Certifier struct {
+	Address     sdk.AccAddress `json:"certifier"`
+	Alias       string         `json:"alias"`
+	Proposer    sdk.AccAddress `json:"proposer"`
+	Description string         `json:"description"`
+}
+
+// Validator is a type for certified validator.
+type Validator struct {
+	PubKey    crypto.PubKey
+	Certifier sdk.AccAddress
+}
+
+// Platform is a genesis type for certified platform of a validator
+type Platform struct {
+	Validator   crypto.PubKey
+	Description string
+}
+
+// RequestContentType is the type for requestContent
+type RequestContentType byte
+
+// RequestContent types
+const (
+	RequestContentTypeNil RequestContentType = iota
+	RequestContentTypeSourceCodeHash
+	RequestContentTypeAddress
+	RequestContentTypeBytecodeHash
+	RequestContentTypeGeneral
+)
+
+// CertificateID is the type for the ID of a certificate.
+type CertificateID string
+
+// Bytes returns the byte array for a certificate ID.
+func (id CertificateID) Bytes() []byte {
+	decoded, err := hex.DecodeString(id.String())
+	if err != nil {
+		panic(err)
+	}
+	return decoded
+}
+
+func (id CertificateID) String() string {
+	return string(id)
+}
+
+// KVPair defines type for the key-value pair.
+type KVPair struct {
+	Key   string `json:"key"`
+	Value string `json:"value"`
+}
+
+// RequestContent defines type for the request content.
+type RequestContent struct {
+	RequestContentType RequestContentType `json:"request_content_type"`
+	RequestContent     string             `json:"request_content"`
+}
+
+// Certificate is the interface for all kinds of certificate
+type Certificate interface {
+	ID() CertificateID
+	Type() CertificateType
+	Certifier() sdk.AccAddress
+	RequestContent() RequestContent
+	CertificateContent() string
+	FormattedCertificateContent() []KVPair
+	Description() string
+	TxHash() string
+
+	Bytes(*codec.LegacyAmino) []byte
+	String() string
+
+	SetCertificateID(CertificateID)
+	SetTxHash(string)
+}
+
+// Library is a type for certified libraries.
+type Library struct {
+	Address   sdk.AccAddress
+	Publisher sdk.AccAddress
+}
+
+var _ Certificate = GeneralCertificate{}
+var _ Certificate = CompilationCertificate{}
+
+// GeneralCertificate defines the type for general certificate.
+type GeneralCertificate struct {
+	CertID          CertificateID   `json:"certificate_id"`
+	CertType        CertificateType `json:"certificate_type"`
+	ReqContent      RequestContent  `json:"request_content"`
+	CertDescription string          `json:"description"`
+	CertCertifier   sdk.AccAddress  `json:"certifier"`
+	CertTxHash      string          `json:"txhash"`
+}
+
+// ID returns ID of the certificate.
+func (c GeneralCertificate) ID() CertificateID {
+	return c.CertID
+}
+
+// Type returns the certificate type.
+func (c GeneralCertificate) Type() CertificateType {
+	return c.CertType
+}
+
+// Certifier returns certifier account address of the certificate.
+func (c GeneralCertificate) Certifier() sdk.AccAddress {
+	return c.CertCertifier
+}
+
+// RequestContent returns request content of the certificate.
+func (c GeneralCertificate) RequestContent() RequestContent {
+	return c.ReqContent
+}
+
+// CertificateContent returns certificate content of the certificate.
+func (c GeneralCertificate) CertificateContent() string {
+	return "general certificate"
+}
+
+// FormattedCertificateContent returns formatted certificate content of the certificate.
+func (c GeneralCertificate) FormattedCertificateContent() []KVPair {
+	return nil
+}
+
+// Description returns description of the certificate.
+func (c GeneralCertificate) Description() string {
+	return c.CertDescription
+}
+
+// TxHash returns the hash of the tx when the certificate is issued.
+func (c GeneralCertificate) TxHash() string {
+	return c.CertTxHash
+}
+
+// Bytes returns a byte array for the certificate.
+func (c GeneralCertificate) Bytes(cdc *codec.LegacyAmino) []byte {
+	return cdc.MustMarshalBinaryLengthPrefixed(c)
+}
+
+// String returns a human readable string representation of the certificate.
+func (c GeneralCertificate) String() string {
+	return ""
+}
+
+// SetCertificateID provides a method to set an ID for the certificate.
+func (c GeneralCertificate) SetCertificateID(id CertificateID) {
+	c.CertID = id
+}
+
+// SetTxHash provides a method to set txhash of the certificate.
+func (c GeneralCertificate) SetTxHash(txhash string) {
+	c.CertTxHash = txhash
+}
+
+// CompilationCertificateContent defines type for the compilation certificate content.
+type CompilationCertificateContent struct {
+	Compiler     string `json:"compiler"`
+	BytecodeHash string `json:"bytecode_hash"`
+}
+
+// CompilationCertificate defines type for the compilation certificate.
+type CompilationCertificate struct {
+	IssueBlockHeight int64                         `json:"time_issued"`
+	CertID           CertificateID                 `json:"certificate_id"`
+	CertType         CertificateType               `json:"certificate_type"`
+	ReqContent       RequestContent                `json:"request_content"`
+	CertContent      CompilationCertificateContent `json:"certificate_content"`
+	CertDescription  string                        `json:"description"`
+	CertCertifier    sdk.AccAddress                `json:"certifier"`
+	CertTxHash       string                        `json:"txhash"`
+}
+
+// ID returns ID of the certificate.
+func (c CompilationCertificate) ID() CertificateID {
+	return c.CertID
+}
+
+// Type returns the certificate type.
+func (c CompilationCertificate) Type() CertificateType {
+	return c.CertType
+}
+
+// Certifier returns certifier account address of the certificate.
+func (c CompilationCertificate) Certifier() sdk.AccAddress {
+	return c.CertCertifier
+}
+
+// RequestContent returns request content of the certificate.
+func (c CompilationCertificate) RequestContent() RequestContent {
+	return c.ReqContent
+}
+
+// CertificateContent returns certificate content of the certificate.
+func (c CompilationCertificate) CertificateContent() string {
+	return ""
+}
+
+// FormattedCertificateContent returns formatted certificate content of the certificate.
+func (c CompilationCertificate) FormattedCertificateContent() []KVPair {
+	return []KVPair{}
+}
+
+// Description returns description of the certificate.
+func (c CompilationCertificate) Description() string {
+	return c.CertDescription
+}
+
+// TxHash returns the hash of the tx when the certificate is issued.
+func (c CompilationCertificate) TxHash() string {
+	return c.CertTxHash
+}
+
+// Bytes returns a byte array for the certificate.
+func (c CompilationCertificate) Bytes(cdc *codec.LegacyAmino) []byte {
+	return cdc.MustMarshalBinaryLengthPrefixed(c)
+}
+
+// String returns a human readable string representation of the certificate.
+func (c CompilationCertificate) String() string {
+	return fmt.Sprintf("Compilation certificate\n"+
+		"Certificate ID: %s\n"+
+		"Certificate type: compilation\n"+
+		"RequestContent:\n%s\n"+
+		"CertificateContent:\n%s\n"+
+		"Description: %s\n"+
+		"Certifier: %s\n"+
+		"TxHash: %s\n",
+		c.CertID.String(), c.ReqContent.RequestContent, c.CertificateContent(),
+		c.Description(), c.CertCertifier.String(), c.CertTxHash)
+}
+
+// SetCertificateID provides a method to set an ID for the certificate.
+func (c CompilationCertificate) SetCertificateID(id CertificateID) {
+	c.CertID = id
+}
+
+// SetTxHash provides a method to set txhash of the certificate.
+func (c CompilationCertificate) SetTxHash(txhash string) {
+	c.CertTxHash = txhash
+}
+
+// CertGenesisState - cert genesis state
+type CertGenesisState struct {
+	Certifiers   []Certifier   `json:"certifiers"`
+	Validators   []Validator   `json:"validators"`
+	Platforms    []Platform    `json:"platforms"`
+	Certificates []Certificate `json:"certificates"`
+	Libraries    []Library     `json:"libraries"`
+}
+
+func RegisterCertLegacyAminoCodec(cdc *codec.LegacyAmino) {
+	cdc.RegisterInterface((*Certificate)(nil), nil)
+	cdc.RegisterConcrete(&GeneralCertificate{}, "cert/GeneralCertificate", nil)
+	cdc.RegisterConcrete(&CompilationCertificate{}, "cert/CompilationCertificate", nil)
+}
+
+func migrateCert(oldGenState CertGenesisState) *certtypes.GenesisState {
+	var newCertifiers []certtypes.Certifier
+	for _, c := range oldGenState.Certifiers {
+		newCertifiers = append(newCertifiers, certtypes.Certifier{
+			Alias:       c.Alias,
+			Address:     c.Address.String(),
+			Description: c.Description,
+			Proposer:    c.Proposer.String(),
+		})
+	}
+
+	var newValidators []certtypes.Validator
+	for _, v := range oldGenState.Validators {
+		pkAny := codectypes.UnsafePackAny(v.PubKey)
+		newValidators = append(newValidators, certtypes.Validator{
+			Pubkey:    pkAny,
+			Certifier: v.Certifier.String(),
+		})
+	}
+
+	var newPlatforms []certtypes.Platform
+	for _, p := range oldGenState.Platforms {
+		valPkAny := codectypes.UnsafePackAny(p.Validator)
+		newPlatforms = append(newPlatforms, certtypes.Platform{
+			ValidatorPubkey: valPkAny,
+			Description:     p.Description,
+		})
+	}
+
+	var newCertificates []codectypes.Any
+	for _, c := range oldGenState.Certificates {
+		var newCert certtypes.Certificate
+		switch certificate := c.(type) {
+		case GeneralCertificate:
+			{
+				reqContent := certtypes.RequestContent{
+					RequestContentType: certtypes.RequestContentTypeGeneral,
+					RequestContent:     c.RequestContent().RequestContent,
+				}
+				newCert = &certtypes.GeneralCertificate{
+					CertId:          certtypes.CertificateID(c.ID()),
+					CertType:        certtypes.CertificateTypeGeneral,
+					ReqContent:      &reqContent,
+					CertDescription: c.Description(),
+					CertCertifier:   c.Certifier().String(),
+					CertTxHash:      c.TxHash(),
+				}
+			}
+		case CompilationCertificate:
+			{
+				reqContent := certtypes.RequestContent{
+					RequestContentType: certtypes.RequestContentTypeSourceCodeHash,
+					RequestContent:     c.RequestContent().RequestContent,
+				}
+				newCert = &certtypes.CompilationCertificate{
+					IssueBlockHeight: certificate.IssueBlockHeight,
+					CertId:           certtypes.CertificateID(certificate.ID()),
+					CertType:         certtypes.CertificateTypeCompilation,
+					ReqContent:       &reqContent,
+					CertContent: &certtypes.CompilationCertificateContent{
+						Compiler:     certificate.CertContent.Compiler,
+						BytecodeHash: certificate.CertContent.BytecodeHash,
+					},
+					CertDescription: certificate.CertDescription,
+					CertCertifier:   certificate.Certifier().String(),
+					CertTxHash:      certificate.TxHash(),
+				}
+			}
+		}
+		certAny := codectypes.UnsafePackAny(newCert)
+		newCertificates = append(newCertificates, *certAny)
+	}
+
+	var newLibraries []certtypes.Library
+	for _, l := range oldGenState.Libraries {
+		newLibraries = append(newLibraries, certtypes.Library{
+			Address:   l.Address.String(),
+			Publisher: l.Publisher.String(),
+		})
+	}
+
+	return &certtypes.GenesisState{
+		Certifiers:   newCertifiers,
+		Validators:   newValidators,
+		Platforms:    newPlatforms,
+		Certificates: newCertificates,
+		Libraries:    newLibraries,
+	}
+}
--- cmd/certikd/cmd/migrate/migrate.go
+++ cmd/certikd/cmd/migrate/migrate.go
@@ -1,14 +1,31 @@
 package migrate
 
 import (
-	cvmtypes "github.com/certikfoundation/shentu/x/cvm/types"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"time"
+
+	shieldtypes "github.com/certikfoundation/shentu/x/shield/types"
+
+	"github.com/spf13/cobra"
+
+	tmjson "github.com/tendermint/tendermint/libs/json"
+	tmtypes "github.com/tendermint/tendermint/types"
+
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/cosmos/cosmos-sdk/version"
 	v039auth "github.com/cosmos/cosmos-sdk/x/auth/legacy/v039"
 	v040auth "github.com/cosmos/cosmos-sdk/x/auth/legacy/v040"
 	v036supply "github.com/cosmos/cosmos-sdk/x/bank/legacy/v036"
 	v038bank "github.com/cosmos/cosmos-sdk/x/bank/legacy/v038"
 	v040bank "github.com/cosmos/cosmos-sdk/x/bank/legacy/v040"
+	bank "github.com/cosmos/cosmos-sdk/x/bank/types"
+	captypes "github.com/cosmos/cosmos-sdk/x/capability/types"
 	v039crisis "github.com/cosmos/cosmos-sdk/x/crisis/legacy/v039"
 	v040crisis "github.com/cosmos/cosmos-sdk/x/crisis/legacy/v040"
 	v036distr "github.com/cosmos/cosmos-sdk/x/distribution/legacy/v036"
@@ -16,12 +33,18 @@ import (
 	v040distr "github.com/cosmos/cosmos-sdk/x/distribution/legacy/v040"
 	v038evidence "github.com/cosmos/cosmos-sdk/x/evidence/legacy/v038"
 	v040evidence "github.com/cosmos/cosmos-sdk/x/evidence/legacy/v040"
+	evtypes "github.com/cosmos/cosmos-sdk/x/evidence/types"
+	"github.com/cosmos/cosmos-sdk/x/genutil/client/cli"
 	v039genutil "github.com/cosmos/cosmos-sdk/x/genutil/legacy/v039"
 	"github.com/cosmos/cosmos-sdk/x/genutil/types"
 	genutiltypes "github.com/cosmos/cosmos-sdk/x/genutil/types"
 	v036gov "github.com/cosmos/cosmos-sdk/x/gov/legacy/v036"
 	v040gov "github.com/cosmos/cosmos-sdk/x/gov/legacy/v040"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
+	ibcxfertypes "github.com/cosmos/cosmos-sdk/x/ibc/applications/transfer/types"
+	host "github.com/cosmos/cosmos-sdk/x/ibc/core/24-host"
+	"github.com/cosmos/cosmos-sdk/x/ibc/core/exported"
+	ibccoretypes "github.com/cosmos/cosmos-sdk/x/ibc/core/types"
 	v039mint "github.com/cosmos/cosmos-sdk/x/mint/legacy/v039"
 	v040mint "github.com/cosmos/cosmos-sdk/x/mint/legacy/v040"
 	v036params "github.com/cosmos/cosmos-sdk/x/params/legacy/v036"
@@ -29,7 +52,18 @@ import (
 	v040slashing "github.com/cosmos/cosmos-sdk/x/slashing/legacy/v040"
 	v038staking "github.com/cosmos/cosmos-sdk/x/staking/legacy/v038"
 	v040staking "github.com/cosmos/cosmos-sdk/x/staking/legacy/v040"
+	staking "github.com/cosmos/cosmos-sdk/x/staking/types"
 	v038upgrade "github.com/cosmos/cosmos-sdk/x/upgrade/legacy/v038"
+
+	certtypes "github.com/certikfoundation/shentu/x/cert/types"
+	cvmtypes "github.com/certikfoundation/shentu/x/cvm/types"
+)
+
+const (
+	flagGenesisTime     = "genesis-time"
+	flagInitialHeight   = "initial-height"
+	flagReplacementKeys = "replacement-cons-keys"
+	flagNoProp29        = "no-prop-29"
 )
 
 func migrateGenutil(oldGenState v039genutil.GenesisState) *types.GenesisState {
@@ -46,6 +80,8 @@ func Migrate(appState types.AppMap, clientCtx client.Context) types.AppMap {
 	v036params.RegisterLegacyAminoCodec(v039Codec)
 	v038upgrade.RegisterLegacyAminoCodec(v039Codec)
 	RegisterCVMLegacyAminoCodec(v039Codec)
+	RegisterCertLegacyAminoCodec(v039Codec)
+	RegisterShieldLegacyAminoCodec(v039Codec)
 
 	v040Codec := clientCtx.JSONMarshaler
 
@@ -199,19 +235,201 @@ func Migrate(appState types.AppMap, clientCtx client.Context) types.AppMap {
 		appState[genutiltypes.ModuleName] = v040Codec.MustMarshalJSON(migrateGenutil(genutilGenState))
 	}
 
-	//Migrate CVM
+	// Migrate x/cvm
 	if appState[cvmtypes.ModuleName] != nil {
 		var cvmGenState CVMGenesisState
 		v039Codec.MustUnmarshalJSON(appState[cvmtypes.ModuleName], &cvmGenState)
 
 		delete(appState, cvmtypes.ModuleName)
 
-		appState[genutiltypes.ModuleName] = v040Codec.MustMarshalJSON(migrateCVM(cvmGenState))
+		appState[cvmtypes.ModuleName] = v040Codec.MustMarshalJSON(migrateCVM(cvmGenState))
 	}
 
-	//Migrate Cert
-	//Migrate Oracle
-	//Migrate Shield
+	// Migrate x/cert
+	if appState[certtypes.ModuleName] != nil {
+		var certGenState CertGenesisState
+		v039Codec.MustUnmarshalJSON(appState[certtypes.ModuleName], &certGenState)
+
+		delete(appState, certtypes.ModuleName)
+
+		appState[certtypes.ModuleName] = v040Codec.MustMarshalJSON(migrateCert(certGenState))
+	}
+
+	// Migrate x/oracle
+
+	// Migrate x/shield
+	if appState[shieldtypes.ModuleName] != nil {
+		var shieldGenState ShieldGenesisState
+		v039Codec.MustUnmarshalJSON(appState[shieldtypes.ModuleName], &shieldGenState)
+
+		delete(appState, shieldtypes.ModuleName)
+
+		appState[shieldtypes.ModuleName] = v040Codec.MustMarshalJSON(migrateShield(shieldGenState))
+	}
 
 	return appState
+}
+
+// MigrateGenesisCmd returns a command to execute genesis state migration.
+func MigrateGenesisCmd() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "migrate [genesis-file]",
+		Short: "Migrate genesis to a specified target version",
+		Long: fmt.Sprintf(`Migrate the source genesis into the target version and print to STDOUT.
+
+Example:
+$ %s migrate /path/to/genesis.json --chain-id=cosmoshub-4 --genesis-time=2019-04-22T17:00:00Z --initial-height=5000
+`, version.AppName),
+		Args: cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			clientCtx := client.GetClientContextFromCmd(cmd)
+
+			var err error
+
+			firstMigration := "v0.38"
+			importGenesis := args[0]
+
+			jsonBlob, err := ioutil.ReadFile(importGenesis)
+
+			if err != nil {
+				return errors.Wrap(err, "failed to read provided genesis file")
+			}
+
+			jsonBlob, err = migrateTendermintGenesis(jsonBlob)
+
+			if err != nil {
+				return errors.Wrap(err, "failed to migration from 0.32 Tendermint params to 0.34 parms")
+			}
+
+			genDoc, err := tmtypes.GenesisDocFromJSON(jsonBlob)
+			if err != nil {
+				return errors.Wrapf(err, "failed to read genesis document from file %s", importGenesis)
+			}
+
+			var initialState types.AppMap
+			if err := json.Unmarshal(genDoc.AppState, &initialState); err != nil {
+				return errors.Wrap(err, "failed to JSON unmarshal initial genesis state")
+			}
+
+			migrationFunc := cli.GetMigrationCallback(firstMigration)
+			if migrationFunc == nil {
+				return fmt.Errorf("unknown migration function for version: %s", firstMigration)
+			}
+
+			// TODO: handler error from migrationFunc call
+			newGenState := migrationFunc(initialState, clientCtx)
+
+			secondMigration := "v0.39"
+
+			migrationFunc = cli.GetMigrationCallback(secondMigration)
+			if migrationFunc == nil {
+				return fmt.Errorf("unknown migration function for version: %s", secondMigration)
+			}
+
+			// TODO: handler error from migrationFunc call
+			newGenState = migrationFunc(newGenState, clientCtx)
+
+			thirdMigration := "v0.40"
+
+			migrationFunc = Migrate
+			if migrationFunc == nil {
+				return fmt.Errorf("unknown migration function for version: %s", thirdMigration)
+			}
+
+			// TODO: handler error from migrationFunc call
+			newGenState = migrationFunc(newGenState, clientCtx)
+
+			var bankGenesis bank.GenesisState
+
+			clientCtx.JSONMarshaler.MustUnmarshalJSON(newGenState[bank.ModuleName], &bankGenesis)
+
+			bankGenesis.DenomMetadata = []bank.Metadata{
+				{
+					Description: "The native staking token of the Cosmos Hub.",
+					DenomUnits: []*bank.DenomUnit{
+						{Denom: "uatom", Exponent: uint32(0), Aliases: []string{"microatom"}},
+						{Denom: "matom", Exponent: uint32(3), Aliases: []string{"milliatom"}},
+						{Denom: "atom", Exponent: uint32(6), Aliases: []string{}},
+					},
+					Base:    "uatom",
+					Display: "atom",
+				},
+			}
+			newGenState[bank.ModuleName] = clientCtx.JSONMarshaler.MustMarshalJSON(&bankGenesis)
+
+			var stakingGenesis staking.GenesisState
+
+			clientCtx.JSONMarshaler.MustUnmarshalJSON(newGenState[staking.ModuleName], &stakingGenesis)
+
+			ibcTransferGenesis := ibcxfertypes.DefaultGenesisState()
+			ibcCoreGenesis := ibccoretypes.DefaultGenesisState()
+			capGenesis := captypes.DefaultGenesis()
+			evGenesis := evtypes.DefaultGenesisState()
+
+			ibcTransferGenesis.Params.ReceiveEnabled = false
+			ibcTransferGenesis.Params.SendEnabled = false
+
+			ibcCoreGenesis.ClientGenesis.Params.AllowedClients = []string{exported.Tendermint}
+			stakingGenesis.Params.HistoricalEntries = 10000
+
+			newGenState[ibcxfertypes.ModuleName] = clientCtx.JSONMarshaler.MustMarshalJSON(ibcTransferGenesis)
+			newGenState[host.ModuleName] = clientCtx.JSONMarshaler.MustMarshalJSON(ibcCoreGenesis)
+			newGenState[captypes.ModuleName] = clientCtx.JSONMarshaler.MustMarshalJSON(capGenesis)
+			newGenState[evtypes.ModuleName] = clientCtx.JSONMarshaler.MustMarshalJSON(evGenesis)
+			newGenState[staking.ModuleName] = clientCtx.JSONMarshaler.MustMarshalJSON(&stakingGenesis)
+
+			genDoc.AppState, err = json.Marshal(newGenState)
+			if err != nil {
+				return errors.Wrap(err, "failed to JSON marshal migrated genesis state")
+			}
+
+			genesisTime, _ := cmd.Flags().GetString(flagGenesisTime)
+			if genesisTime != "" {
+				var t time.Time
+
+				err := t.UnmarshalText([]byte(genesisTime))
+				if err != nil {
+					return errors.Wrap(err, "failed to unmarshal genesis time")
+				}
+
+				genDoc.GenesisTime = t
+			}
+
+			chainID, _ := cmd.Flags().GetString(flags.FlagChainID)
+			if chainID != "" {
+				genDoc.ChainID = chainID
+			}
+
+			initialHeight, _ := cmd.Flags().GetInt(flagInitialHeight)
+
+			genDoc.InitialHeight = int64(initialHeight)
+
+			replacementKeys, _ := cmd.Flags().GetString(flagReplacementKeys)
+
+			if replacementKeys != "" {
+				genDoc = loadKeydataFromFile(clientCtx, replacementKeys, genDoc)
+			}
+
+			bz, err := tmjson.Marshal(genDoc)
+			if err != nil {
+				return errors.Wrap(err, "failed to marshal genesis doc")
+			}
+
+			sortedBz, err := sdk.SortJSON(bz)
+			if err != nil {
+				return errors.Wrap(err, "failed to sort JSON genesis doc")
+			}
+
+			fmt.Println(string(sortedBz))
+			return nil
+		},
+	}
+
+	cmd.Flags().String(flagGenesisTime, "", "override genesis_time with this flag")
+	cmd.Flags().Int(flagInitialHeight, 0, "Set the starting height for the chain")
+	cmd.Flags().String(flagReplacementKeys, "", "Proviide a JSON file to replace the consensus keys of validators")
+	cmd.Flags().String(flags.FlagChainID, "", "override chain_id with this flag")
+	cmd.Flags().Bool(flagNoProp29, false, "Do not implement fund recovery from prop29")
+
+	return cmd
 }
