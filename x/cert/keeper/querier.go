b616d7218c8f2e21ce4807d31f1d51164b600f13
hacheigriega <hykim.home@gmail.com>
2021-04-08 11:21:57 +0900

    fix merge errors

--- x/cert/keeper/querier.go
+++ x/cert/keeper/querier.go
@@ -128,8 +128,8 @@ func queryPlatform(ctx sdk.Context, path []string, keeper Keeper, legacyQuerierC
 		return []byte{}, sdkerrors.Wrapf(sdkerrors.ErrUnknownRequest, "Expecting 1 arg. Found %d.", len(path))
 	}
 
-  validator, err := sdk.GetPubKeyFromBech32(sdk.Bech32PubKeyTypeConsPub, path[0])
-	if err1 != nil {
+	validator, err := sdk.GetPubKeyFromBech32(sdk.Bech32PubKeyTypeConsPub, path[0])
+	if err != nil {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrInvalidPubKey, path[0])
 	}
 
@@ -139,7 +139,7 @@ func queryPlatform(ctx sdk.Context, path []string, keeper Keeper, legacyQuerierC
 	}
 
 	res, err = codec.MarshalJSONIndent(legacyQuerierCdc, platform)
-	if err2 != nil {
+	if err != nil {
 		return nil, sdkerrors.Wrap(sdkerrors.ErrJSONMarshal, err.Error())
 	}
 
