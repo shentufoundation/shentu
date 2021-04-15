fbf367a9b843c49df3880d5607b26756e5503e63
Michael Park <michael.park@certik.org>
2021-04-14 21:58:49 -0700

    oracle: refactored tests for clarity

--- x/oracle/keeper/operator_test.go
+++ x/oracle/keeper/operator_test.go
@@ -11,89 +11,114 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/certikfoundation/shentu/simapp"
-	"github.com/certikfoundation/shentu/x/oracle/types"
 )
 
-func TestOperator_Basic(t *testing.T) {
+func TestOperatorBasic(t *testing.T) {
 	app := simapp.Setup(false)
 	ctx := app.BaseApp.NewContext(false, tmproto.Header{Time: time.Now().UTC()})
-	addrs := simapp.AddTestAddrs(app, ctx, 3, sdk.NewInt(80000*1e6))
+	addrs := simapp.AddTestAddrs(app, ctx, 4, sdk.NewInt(80000*1e6))
 	ok := app.OracleKeeper
 
 	params := ok.GetLockedPoolParams(ctx)
+	collateral := sdk.Coins{sdk.NewInt64Coin("uctk", params.MinimumCollateral)}
 
-	err := ok.CreateOperator(ctx, addrs[1], sdk.Coins{sdk.NewInt64Coin("uctk", params.MinimumCollateral)}, addrs[0], "operator1")
-	require.Nil(t, err)
+	require.NoError(t, ok.CreateOperator(ctx, addrs[0], collateral, addrs[1], "operator1"))
+	require.NoError(t, ok.CreateOperator(ctx, addrs[2], collateral, addrs[3], "operator2"))
 
-	operator1, err := ok.GetOperator(ctx, addrs[1])
+	operator1, err := ok.GetOperator(ctx, addrs[0])
 	require.Nil(t, err)
-
-	err = ok.CreateOperator(ctx, addrs[2], sdk.Coins{sdk.NewInt64Coin("uctk", params.MinimumCollateral)}, addrs[0], "operator2")
-	require.Nil(t, err)
+	require.Equal(t, addrs[0].String(), operator1.Address)
+	require.Equal(t, collateral, operator1.Collateral)
+	require.Equal(t, addrs[1].String(), operator1.Proposer)
 
 	operator2, err := ok.GetOperator(ctx, addrs[2])
 	require.Nil(t, err)
+	require.Equal(t, addrs[2].String(), operator2.Address)
+	require.Equal(t, collateral, operator2.Collateral)
+	require.Equal(t, addrs[3].String(), operator2.Proposer)
 
-	require.Equal(t, types.Operators{operator1, operator2}, ok.GetAllOperators(ctx))
+	operators := ok.GetAllOperators(ctx)
+	require.Len(t, operators, 2)
 
-	err = ok.RemoveOperator(ctx, addrs[1])
-	require.Nil(t, err)
+	require.NoError(t, ok.RemoveOperator(ctx, addrs[0]))
 
-	require.Equal(t, types.Operators{operator2}, ok.GetAllOperators(ctx))
+	operators = ok.GetAllOperators(ctx)
+	require.Len(t, operators, 1)
+	require.Equal(t, operator2, operators[0])
 }
 
-func TestOperator_Collateral(t *testing.T) {
+func TestOperatorCollateral(t *testing.T) {
 	app := simapp.Setup(false)
 	ctx := app.BaseApp.NewContext(false, tmproto.Header{Time: time.Now().UTC()})
 	addrs := simapp.AddTestAddrs(app, ctx, 2, sdk.NewInt(80000*1e6))
 	ok := app.OracleKeeper
 
 	params := ok.GetLockedPoolParams(ctx)
+	minCollateral := sdk.Coins{sdk.NewInt64Coin("uctk", params.MinimumCollateral)}
+	hundred := sdk.Coins{sdk.NewInt64Coin("uctk", 100)}
 
-	err := ok.CreateOperator(ctx, addrs[0], sdk.Coins{sdk.NewInt64Coin("uctk", params.MinimumCollateral)}, addrs[1], "operator1")
-	require.Nil(t, err)
+	require.NoError(t, ok.CreateOperator(ctx, addrs[0], minCollateral, addrs[1], "operator1"))
 
 	collateral, err := ok.GetCollateralAmount(ctx, addrs[0])
 	require.Nil(t, err)
 	require.Equal(t, sdk.NewInt(params.MinimumCollateral), collateral)
 
-	err = ok.AddCollateral(ctx, addrs[0], sdk.Coins{sdk.NewInt64Coin("uctk", 100)})
-	require.Nil(t, err)
+	require.NoError(t, ok.AddCollateral(ctx, addrs[0], hundred))
 
 	collateral, err = ok.GetCollateralAmount(ctx, addrs[0])
 	require.Nil(t, err)
 	require.Equal(t, sdk.NewInt(params.MinimumCollateral+100), collateral)
 
-	err = ok.ReduceCollateral(ctx, addrs[0], sdk.Coins{sdk.NewInt64Coin("uctk", 100)})
-	require.Nil(t, err)
+	require.NoError(t, ok.ReduceCollateral(ctx, addrs[0], hundred))
 
 	collateral, err = ok.GetCollateralAmount(ctx, addrs[0])
 	require.Nil(t, err)
 	require.Equal(t, sdk.NewInt(params.MinimumCollateral), collateral)
 
+	require.Error(t, ok.ReduceCollateral(ctx, addrs[0], hundred))
+	require.Error(t, ok.AddCollateral(ctx, addrs[1], hundred))
+
 	withdraws := ok.GetAllWithdraws(ctx)
-	require.Equal(t, 1, len(withdraws))
+	require.Len(t, withdraws, 1)
 }
 
-func TestOperator_Reward(t *testing.T) {
+func TestOperatorReward(t *testing.T) {
 	app := simapp.Setup(false)
 	ctx := app.BaseApp.NewContext(false, tmproto.Header{Time: time.Now().UTC()})
 	addrs := simapp.AddTestAddrs(app, ctx, 2, sdk.NewInt(80000*1e6))
 	ok := app.OracleKeeper
 
 	params := ok.GetLockedPoolParams(ctx)
+	minCollateral := sdk.Coins{sdk.NewInt64Coin("uctk", params.MinimumCollateral)}
 
-	err := ok.CreateOperator(ctx, addrs[0], sdk.Coins{sdk.NewInt64Coin("uctk", params.MinimumCollateral)}, addrs[1], "operator1")
-	require.Nil(t, err)
-
-	err = ok.AddReward(ctx, addrs[0], sdk.Coins{sdk.NewInt64Coin("uctk", 200)})
-	require.Nil(t, err)
+	require.NoError(t, ok.CreateOperator(ctx, addrs[0], minCollateral, addrs[1], "operator1"))
 
 	operator, err := ok.GetOperator(ctx, addrs[0])
 	require.Nil(t, err)
-	require.Equal(t, sdk.Coins{sdk.NewInt64Coin("uctk", 200)}, operator.AccumulatedRewards)
+	require.Equal(t, addrs[0].String(), operator.Address)
+	require.Equal(t, minCollateral, operator.Collateral)
+	require.Equal(t, addrs[1].String(), operator.Proposer)
+	require.Nil(t, operator.AccumulatedRewards)
+
+	require.NoError(t, ok.AddReward(ctx, addrs[0], sdk.Coins{sdk.NewInt64Coin("uctk", 100)}))
+	require.NoError(t, ok.AddReward(ctx, addrs[0], sdk.Coins{sdk.NewInt64Coin("uctk", 200)}))
+	require.Error(t, ok.AddReward(ctx, addrs[1], sdk.Coins{sdk.NewInt64Coin("uctk", 300)}))
+
+	operator, err = ok.GetOperator(ctx, addrs[0])
+	require.Nil(t, err)
+	require.Equal(t, addrs[0].String(), operator.Address)
+	require.Equal(t, minCollateral, operator.Collateral)
+	require.Equal(t, addrs[1].String(), operator.Proposer)
+	require.Equal(t, sdk.Coins{sdk.NewInt64Coin("uctk", 300)}, operator.AccumulatedRewards)
 
 	withdrawal, err := ok.WithdrawAllReward(ctx, addrs[0])
 	require.Nil(t, err)
-	require.Equal(t, sdk.Coins{sdk.NewInt64Coin("uctk", 200)}, withdrawal)
+	require.Equal(t, sdk.Coins{sdk.NewInt64Coin("uctk", 300)}, withdrawal)
+
+	operator, err = ok.GetOperator(ctx, addrs[0])
+	require.Nil(t, err)
+	require.Equal(t, addrs[0].String(), operator.Address)
+	require.Equal(t, minCollateral, operator.Collateral)
+	require.Equal(t, addrs[1].String(), operator.Proposer)
+	require.Nil(t, operator.AccumulatedRewards)
 }
--- x/oracle/keeper/task_test.go
+++ x/oracle/keeper/task_test.go
@@ -14,141 +14,256 @@ import (
 	"github.com/certikfoundation/shentu/x/oracle/types"
 )
 
-func TestTask_Basic(t *testing.T) {
+func TestTaskBasic(t *testing.T) {
 	app := simapp.Setup(false)
 	ctx := app.BaseApp.NewContext(false, tmproto.Header{Time: time.Now().UTC()})
 	addrs := simapp.AddTestAddrs(app, ctx, 1, sdk.NewInt(80000*1e6))
 	ok := app.OracleKeeper
 
-	contract1 := "0x1234567890abcdef"
-	function1 := "func1"
-	bounty := sdk.Coins{sdk.NewInt64Coin("uctk", 5000)}
+	bounty := sdk.Coins{sdk.NewInt64Coin("uctk", 100000)}
 	description := "testing"
-	expiration := time.Now().Add(time.Hour).UTC()
 	waitingBlocks := int64(5)
 
-	err := ok.CreateTask(ctx, contract1, function1, bounty, description, expiration, addrs[0], waitingBlocks)
-	require.Nil(t, err)
+	contract1 := "0x1234567890abcdef"
+	function1 := "func1"
+	expiration1 := time.Now().Add(time.Hour).UTC()
+	require.NoError(t, ok.CreateTask(ctx, contract1, function1, bounty, description, expiration1, addrs[0], waitingBlocks))
 
 	task1, err := ok.GetTask(ctx, contract1, function1)
 	require.Nil(t, err)
+	require.Equal(t, contract1, task1.Contract)
+	require.Equal(t, function1, task1.Function)
+	require.Equal(t, expiration1, task1.Expiration)
 
 	contract2 := "0x1234567890fedcba"
 	function2 := "func2"
-	err = ok.CreateTask(ctx, contract2, function2, bounty, description, expiration, addrs[0], waitingBlocks)
-	require.Nil(t, err)
+	expiration2 := time.Now().Add(time.Hour * 2).UTC()
+	require.NoError(t, ok.CreateTask(ctx, contract2, function2, bounty, description, expiration2, addrs[0], waitingBlocks))
 
 	task2, err := ok.GetTask(ctx, contract2, function2)
 	require.Nil(t, err)
+	require.Equal(t, contract2, task2.Contract)
+	require.Equal(t, function2, task2.Function)
+	require.Equal(t, expiration2, task2.Expiration)
 
-	require.Equal(t, []types.Task{task1, task2}, ok.GetAllTasks(ctx))
+	tasks := ok.GetAllTasks(ctx)
+	require.Len(t, tasks, 2)
 
-	err = ok.RemoveTask(ctx, contract1, function1, true, addrs[0])
-	require.Error(t, err)
+	require.Error(t, ok.RemoveTask(ctx, contract1, function1, false, addrs[0]))
+	require.Error(t, ok.RemoveTask(ctx, contract2, function2, false, addrs[0]))
 
-	ctx = ctx.WithBlockHeight(6)
-	err = ok.RemoveTask(ctx, contract1, function1, true, addrs[0])
-	require.Nil(t, err)
+	ctx = ctx.WithBlockTime(expiration2)
+	require.Error(t, ok.RemoveTask(ctx, contract1, function1, false, addrs[0]))
+	require.Error(t, ok.RemoveTask(ctx, contract2, function2, false, addrs[0]))
 
+	ctx = ctx.WithBlockHeight(ctx.BlockHeight() + 6)
+	require.NoError(t, ok.RemoveTask(ctx, contract1, function1, false, addrs[0]))
+	require.Error(t, ok.RemoveTask(ctx, contract2, function2, false, addrs[0]))
+
+	tasks = ok.GetAllTasks(ctx)
+	require.Len(t, tasks, 1)
 	require.Equal(t, []types.Task{task2}, ok.GetAllTasks(ctx))
 }
 
-func TestTask_MinScore(t *testing.T) {
+func TestTaskAggregateFail(t *testing.T) {
 	app := simapp.Setup(false)
 	ctx := app.BaseApp.NewContext(false, tmproto.Header{Time: time.Now().UTC()})
-	addrs := simapp.AddTestAddrs(app, ctx, 3, sdk.NewInt(80000*1e6))
+	addrs := simapp.AddTestAddrs(app, ctx, 4, sdk.NewInt(80000*1e6))
 	ok := app.OracleKeeper
 
 	params := ok.GetLockedPoolParams(ctx)
+	collateral := sdk.Coins{sdk.NewInt64Coin("uctk", params.MinimumCollateral)}
 
-	err := ok.CreateOperator(ctx, addrs[1], sdk.Coins{sdk.NewInt64Coin("uctk", params.MinimumCollateral)}, addrs[0], "operator1")
-	require.Nil(t, err)
-
-	err = ok.CreateOperator(ctx, addrs[2], sdk.Coins{sdk.NewInt64Coin("uctk", params.MinimumCollateral)}, addrs[0], "operator1")
-	require.Nil(t, err)
+	require.NoError(t, ok.CreateOperator(ctx, addrs[0], collateral, addrs[1], "operator1"))
+	require.NoError(t, ok.CreateOperator(ctx, addrs[2], collateral, addrs[3], "operator2"))
 
 	contract := "0x1234567890abcdef"
 	function := "func"
-	bounty := sdk.Coins{sdk.NewInt64Coin("uctk", 5000)}
+	bounty := sdk.Coins{sdk.NewInt64Coin("uctk", 100000)}
 	description := "testing"
 	expiration := time.Now().Add(time.Hour).UTC()
 	waitingBlocks := int64(5)
 
-	err = ok.CreateTask(ctx, contract, function, bounty, description, expiration, addrs[0], waitingBlocks)
-	require.Nil(t, err)
-
-	err = ok.RespondToTask(ctx, contract, function, 100, addrs[1])
-	require.Nil(t, err)
-
-	err = ok.RespondToTask(ctx, contract, function, 0, addrs[2])
-	require.Nil(t, err)
-
-	err = ok.Aggregate(ctx, contract, function)
-	require.Nil(t, err)
+	require.NoError(t, ok.CreateTask(ctx, contract, function, bounty, description, expiration, addrs[0], waitingBlocks))
 
 	task, err := ok.GetTask(ctx, contract, function)
 	require.Nil(t, err)
+	require.Equal(t, contract, task.Contract)
+	require.Equal(t, function, task.Function)
+
+	require.NoError(t, ok.RespondToTask(ctx, contract, function, 100, addrs[0]))
+	require.NoError(t, ok.RespondToTask(ctx, contract, function, 100, addrs[2]))
+
+	ctx = ctx.WithBlockHeight(ctx.BlockHeight() + 6)
+	require.Error(t, ok.RespondToTask(ctx, contract, function, 100, addrs[0]))
+
+	ok.UpdateAndSetTask(ctx, task)
+	task.Status = types.TaskStatusFailed
+	ok.SetTask(ctx, task)
+	require.Error(t, ok.Aggregate(ctx, contract, function))
+
+	task.Status = types.TaskStatusPending
+	ok.SetTask(ctx, task)
+	require.NoError(t, ok.Aggregate(ctx, contract, function))
+}
+
+func TestTaskNoResponses(t *testing.T) {
+	app := simapp.Setup(false)
+	ctx := app.BaseApp.NewContext(false, tmproto.Header{Time: time.Now().UTC()})
+	addrs := simapp.AddTestAddrs(app, ctx, 1, sdk.NewInt(80000*1e6))
+	ok := app.OracleKeeper
+
+	contract := "0x1234567890abcdef"
+	function := "func"
+	bounty := sdk.Coins{sdk.NewInt64Coin("uctk", 100000)}
+	description := "testing"
+	expiration := time.Now().Add(time.Hour).UTC()
+	waitingBlocks := int64(5)
+
+	require.NoError(t, ok.CreateTask(ctx, contract, function, bounty, description, expiration, addrs[0], waitingBlocks))
+	require.NoError(t, ok.Aggregate(ctx, contract, function))
+
+	task, err := ok.GetTask(ctx, contract, function)
+	require.Nil(t, err)
+	require.Equal(t, contract, task.Contract)
+	require.Equal(t, function, task.Function)
+	require.Equal(t, types.TaskStatusFailed, task.Status)
+}
+
+func TestTaskMinScore(t *testing.T) {
+	app := simapp.Setup(false)
+	ctx := app.BaseApp.NewContext(false, tmproto.Header{Time: time.Now().UTC()})
+	addrs := simapp.AddTestAddrs(app, ctx, 4, sdk.NewInt(80000*1e6))
+	ok := app.OracleKeeper
+
+	params := ok.GetLockedPoolParams(ctx)
+	collateral := sdk.Coins{sdk.NewInt64Coin("uctk", params.MinimumCollateral)}
+
+	require.NoError(t, ok.CreateOperator(ctx, addrs[0], collateral, addrs[1], "operator1"))
+	require.NoError(t, ok.CreateOperator(ctx, addrs[2], collateral, addrs[3], "operator2"))
+
+	contract := "0x1234567890abcdef"
+	function := "func"
+	bounty := sdk.Coins{sdk.NewInt64Coin("uctk", 100000)}
+	description := "testing"
+	expiration := time.Now().Add(time.Hour).UTC()
+	waitingBlocks := int64(5)
+
+	require.NoError(t, ok.CreateTask(ctx, contract, function, bounty, description, expiration, addrs[0], waitingBlocks))
+
+	require.NoError(t, ok.RespondToTask(ctx, contract, function, 100, addrs[0]))
+	require.NoError(t, ok.RespondToTask(ctx, contract, function, 0, addrs[2]))
+
+	require.NoError(t, ok.Aggregate(ctx, contract, function))
+
+	task, err := ok.GetTask(ctx, contract, function)
+	require.Nil(t, err)
+	require.Equal(t, contract, task.Contract)
+	require.Equal(t, function, task.Function)
 	require.Equal(t, types.TaskStatusSucceeded, task.Status)
 	require.Equal(t, sdk.NewInt(params.MinimumCollateral), task.Result)
 
-	err = ok.DistributeBounty(ctx, task)
-	require.Nil(t, err)
+	require.NoError(t, ok.DistributeBounty(ctx, task))
 
-	operator1, err := ok.GetOperator(ctx, addrs[1])
+	operator1, err := ok.GetOperator(ctx, addrs[0])
 	require.Nil(t, err)
+	require.Equal(t, addrs[0].String(), operator1.Address)
 	require.Equal(t, bounty, operator1.AccumulatedRewards)
 
 	operator2, err := ok.GetOperator(ctx, addrs[2])
 	require.Nil(t, err)
+	require.Equal(t, addrs[2].String(), operator2.Address)
 	require.Nil(t, operator2.AccumulatedRewards)
 }
 
-func TestTask_AboveThreshold(t *testing.T) {
+func TestTaskBelowThreshold(t *testing.T) {
 	app := simapp.Setup(false)
 	ctx := app.BaseApp.NewContext(false, tmproto.Header{Time: time.Now().UTC()})
-	addrs := simapp.AddTestAddrs(app, ctx, 3, sdk.NewInt(80000*1e6))
+	addrs := simapp.AddTestAddrs(app, ctx, 4, sdk.NewInt(80000*1e6))
 	ok := app.OracleKeeper
 
 	params := ok.GetLockedPoolParams(ctx)
+	collateral := sdk.Coins{sdk.NewInt64Coin("uctk", params.MinimumCollateral)}
 
-	err := ok.CreateOperator(ctx, addrs[1], sdk.Coins{sdk.NewInt64Coin("uctk", params.MinimumCollateral)}, addrs[0], "operator1")
-	require.Nil(t, err)
-
-	err = ok.CreateOperator(ctx, addrs[2], sdk.Coins{sdk.NewInt64Coin("uctk", params.MinimumCollateral)}, addrs[0], "operator1")
-	require.Nil(t, err)
+	require.NoError(t, ok.CreateOperator(ctx, addrs[0], collateral, addrs[1], "operator1"))
+	require.NoError(t, ok.CreateOperator(ctx, addrs[2], collateral, addrs[3], "operator2"))
 
 	contract := "0x1234567890abcdef"
 	function := "func"
-	bounty := sdk.Coins{sdk.NewInt64Coin("uctk", 5000)}
+	bounty := sdk.Coins{sdk.NewInt64Coin("uctk", 100000)}
 	description := "testing"
 	expiration := time.Now().Add(time.Hour).UTC()
 	waitingBlocks := int64(5)
 
-	err = ok.CreateTask(ctx, contract, function, bounty, description, expiration, addrs[0], waitingBlocks)
-	require.Nil(t, err)
+	require.NoError(t, ok.CreateTask(ctx, contract, function, bounty, description, expiration, addrs[0], waitingBlocks))
 
-	err = ok.RespondToTask(ctx, contract, function, 80, addrs[1])
-	require.Nil(t, err)
+	require.NoError(t, ok.RespondToTask(ctx, contract, function, 40, addrs[0]))
+	require.NoError(t, ok.RespondToTask(ctx, contract, function, 20, addrs[2]))
 
-	err = ok.RespondToTask(ctx, contract, function, 70, addrs[2])
-	require.Nil(t, err)
-
-	err = ok.Aggregate(ctx, contract, function)
-	require.Nil(t, err)
+	require.NoError(t, ok.Aggregate(ctx, contract, function))
 
 	task, err := ok.GetTask(ctx, contract, function)
 	require.Nil(t, err)
+	require.Equal(t, contract, task.Contract)
+	require.Equal(t, function, task.Function)
 	require.Equal(t, types.TaskStatusSucceeded, task.Status)
-	require.Equal(t, sdk.NewInt(75), task.Result)
+	require.Equal(t, sdk.NewInt(30), task.Result)
 
-	err = ok.DistributeBounty(ctx, task)
-	require.Nil(t, err)
+	require.NoError(t, ok.DistributeBounty(ctx, task))
 
-	operator1, err := ok.GetOperator(ctx, addrs[1])
+	operator1, err := ok.GetOperator(ctx, addrs[0])
 	require.Nil(t, err)
+	require.Equal(t, addrs[0].String(), operator1.Address)
+	require.Equal(t, sdk.Coins{sdk.NewInt64Coin("uctk", 33870)}, operator1.AccumulatedRewards)
 
 	operator2, err := ok.GetOperator(ctx, addrs[2])
 	require.Nil(t, err)
+	require.Equal(t, addrs[2].String(), operator2.Address)
+	require.Equal(t, sdk.Coins{sdk.NewInt64Coin("uctk", 66129)}, operator2.AccumulatedRewards)
+}
 
-	require.Equal(t, bounty, operator1.AccumulatedRewards.Add(operator2.AccumulatedRewards[0]))
+func TestTaskAboveThreshold(t *testing.T) {
+	app := simapp.Setup(false)
+	ctx := app.BaseApp.NewContext(false, tmproto.Header{Time: time.Now().UTC()})
+	addrs := simapp.AddTestAddrs(app, ctx, 4, sdk.NewInt(80000*1e6))
+	ok := app.OracleKeeper
+
+	params := ok.GetLockedPoolParams(ctx)
+	collateral := sdk.Coins{sdk.NewInt64Coin("uctk", params.MinimumCollateral)}
+
+	require.NoError(t, ok.CreateOperator(ctx, addrs[0], collateral, addrs[1], "operator1"))
+	require.NoError(t, ok.CreateOperator(ctx, addrs[2], collateral, addrs[3], "operator2"))
+
+	contract := "0x1234567890abcdef"
+	function := "func"
+	bounty := sdk.Coins{sdk.NewInt64Coin("uctk", 100000)}
+	description := "testing"
+	expiration := time.Now().Add(time.Hour).UTC()
+	waitingBlocks := int64(5)
+
+	require.NoError(t, ok.CreateTask(ctx, contract, function, bounty, description, expiration, addrs[0], waitingBlocks))
+
+	require.NoError(t, ok.RespondToTask(ctx, contract, function, 100, addrs[0]))
+	require.NoError(t, ok.RespondToTask(ctx, contract, function, 60, addrs[2]))
+
+	require.NoError(t, ok.Aggregate(ctx, contract, function))
+
+	task, err := ok.GetTask(ctx, contract, function)
+	require.Nil(t, err)
+	require.Equal(t, contract, task.Contract)
+	require.Equal(t, function, task.Function)
+	require.Equal(t, types.TaskStatusSucceeded, task.Status)
+	require.Equal(t, sdk.NewInt(80), task.Result)
+
+	require.NoError(t, ok.DistributeBounty(ctx, task))
+
+	operator1, err := ok.GetOperator(ctx, addrs[0])
+	require.Nil(t, err)
+	require.Equal(t, addrs[0].String(), operator1.Address)
+	require.Equal(t, sdk.Coins{sdk.NewInt64Coin("uctk", 58333)}, operator1.AccumulatedRewards)
+
+	operator2, err := ok.GetOperator(ctx, addrs[2])
+	require.Nil(t, err)
+	require.Equal(t, addrs[2].String(), operator2.Address)
+	require.Equal(t, sdk.Coins{sdk.NewInt64Coin("uctk", 41666)}, operator2.AccumulatedRewards)
 }
