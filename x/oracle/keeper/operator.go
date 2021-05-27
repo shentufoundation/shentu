daf7cef18af6895d0524be2ae35b1cd2ca6f732d
Xin Hu <hsinhu@umich.edu>
2021-05-27 18:35:08 -0400

    Update operator.go

--- x/oracle/keeper/operator.go
+++ x/oracle/keeper/operator.go
@@ -176,7 +176,6 @@ func (k Keeper) AddReward(ctx sdk.Context, address sdk.AccAddress, increment sdk
 	}
 	operator.AccumulatedRewards = operator.AccumulatedRewards.Add(increment...)
 	k.SetOperator(ctx, operator)
-
 	if err := k.bankKeeper.SendCoinsFromAccountToModule(ctx, address, types.ModuleName, increment); err != nil {
 		return err
 	}
