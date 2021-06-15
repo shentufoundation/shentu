ff04471990922a635c2e34beb5d487e82f7a5e11
hacheigriega <hykim.home@gmail.com>
2021-06-15 13:51:44 -0400

    fix params test

--- x/oracle/types/params_test.go
+++ x/oracle/types/params_test.go
@@ -14,17 +14,17 @@ import (
 func Test_TaskParams(t *testing.T) {
 	p1 := types.DefaultTaskParams()
 	p2 := types.DefaultTaskParams()
-	require.True(t, reflect.DeepEqual(p1, p2))
+	p3 := types.NewTaskParams(time.Duration(24)*time.Hour, int64(40), sdk.NewInt(40), sdk.NewInt(40), sdk.NewInt(2), sdk.NewInt(200))
 
-	p2 = types.NewTaskParams(time.Duration(24)*time.Hour, int64(40), sdk.NewInt(40), sdk.NewInt(40), sdk.NewInt(2), sdk.NewInt(200))
-	require.False(t, reflect.DeepEqual(p1, p2))
+	require.True(t, reflect.DeepEqual(p1, p2))
+	require.False(t, reflect.DeepEqual(p1, p3))
 }
 
 func Test_LockedPoolParams(t *testing.T) {
 	p1 := types.DefaultLockedPoolParams()
 	p2 := types.DefaultLockedPoolParams()
-	require.True(t, reflect.DeepEqual(p1, p2))
+	p3 := types.NewLockedPoolParams(20, 4000)
 
-	p2 = types.NewLockedPoolParams(20, 4000)
-	require.False(t, reflect.DeepEqual(p1, p2))
+	require.True(t, reflect.DeepEqual(p1, p2))
+	require.False(t, reflect.DeepEqual(p1, p3))
 }
