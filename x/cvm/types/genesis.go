b6c9445b6741489ef10a983c50ed15523c614416
hacheigriega <hykim.home@gmail.com>
2021-06-29 17:26:49 -0400

    implement cvm validate genesis

--- x/cvm/types/genesis.go
+++ x/cvm/types/genesis.go
@@ -1,7 +1,10 @@
 package types
 
 import (
-	"encoding/json"
+	fmt "fmt"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/hyperledger/burrow/crypto"
 )
 
 type Contracts = []Contract
@@ -22,12 +25,23 @@ func DefaultGenesisState() *GenesisState {
 	}
 }
 
-// ValidateGenesis validates cvm genesis data.
-func ValidateGenesis(bz json.RawMessage) error {
-	var data GenesisState
-	err := ModuleCdc.UnmarshalJSON(bz, &data)
-	if err != nil {
-		return err
+// Validate performs validation of cvm genesis data.
+func (gs GenesisState) Validate() error {
+	if gs.GasRate > 100 {
+		return fmt.Errorf("failed to validate %s genesis state: GasRate must ???", ModuleName)
+	}
+
+	for _, contract := range gs.Contracts {
+		if len(contract.Address) != crypto.AddressLength {
+			return fmt.Errorf("failed to validate %s genesis state: Incorrect contract address length %s", ModuleName, sdk.AccAddress(contract.Address.Bytes()).String())
+		}
+	}
+
+	for _, metadata := range gs.Metadatas {
+		if len(metadata.Hash) != 32 {
+			return fmt.Errorf("failed to validate %s genesis state: A metadata hash is not 256 bits", ModuleName)
+		}
 	}
 	return nil
+
 }
