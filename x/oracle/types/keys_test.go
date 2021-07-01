ecd7fba16cf76eb20712e8016d99bae6207be45c
yoongbok-lee <52583590+yoongbok-lee@users.noreply.github.com>
2021-07-01 15:26:22 -0400

    Oracle: Improve Oracle Testing (#299)
    * Oracle: Improve Oracle Testing (#292)
    
    * revert operator reward addition
    
    * delete msgs_test.go. refactor tests. lint.
    
    * add msgs_test.go
    
    * fix params test
    
    * run lint
    
    * fix sim
    
    * add missing file: msgs_test.go
    
    Co-authored-by: Xin Hu <54917112+hsinhu@users.noreply.github.com>
    Co-authored-by: hacheigriega <hykim.home@gmail.com>

---
+++ x/oracle/types/keys_test.go
@@ -0,0 +1,55 @@
+package types_test
+
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/test-go/testify/assert"
+
+	"github.com/certikfoundation/shentu/x/oracle/types"
+)
+
+//test OperatorStoreKey
+func Test_OperatorStoreKey(t *testing.T) {
+	t.Run("test ", func(t *testing.T) {
+		acc := sdk.AccAddress([]byte{10})
+		tmp := types.OperatorStoreKey(acc)
+		assert.Equal(t, tmp, []byte{1, 10})
+	})
+}
+
+//test WithdrawStoreKey
+func Test_WithdrawStoreKey(t *testing.T) {
+	t.Run("test ", func(t *testing.T) {
+		acc := sdk.AccAddress([]byte{10})
+		var n int64 = 34
+		tmp := types.WithdrawStoreKey(acc, n)
+		assert.Equal(t, tmp, []byte{2, 34, 0, 0, 0, 0, 0, 0, 0, 10})
+	})
+}
+
+func Test_TotalCollateralKey(t *testing.T) {
+	t.Run("test ", func(t *testing.T) {
+		tmp := types.TotalCollateralKey()
+		assert.Equal(t, tmp, []byte{3})
+	})
+}
+
+//test TaskStoreKey
+func Test_TaskStoreKey(t *testing.T) {
+	t.Run("test ", func(t *testing.T) {
+		s1 := "abc"
+		s2 := "ghj"
+		tmp := types.TaskStoreKey(s1, s2)
+		assert.Equal(t, tmp, []byte{4, 97, 98, 99, 103, 104, 106})
+	})
+}
+
+//test ClosingTaskIDsStoreKey
+func Test_ClosingTaskIDsStoreKey(t *testing.T) {
+	t.Run("test ", func(t *testing.T) {
+		var n int64 = 34
+		tmp := types.ClosingTaskIDsStoreKey(n)
+		assert.Equal(t, tmp, []byte{5, 34, 0, 0, 0, 0, 0, 0, 0})
+	})
+}
