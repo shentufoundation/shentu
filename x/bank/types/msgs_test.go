48c6e3e33630806bb8b0ce12389bb28110d099dc
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2021-11-15 16:29:30 +0100

    refactoring MsgSendRoute unit test on the types bank module

--- x/bank/types/msgs_test.go
+++ x/bank/types/msgs_test.go
@@ -4,20 +4,51 @@ import (
 	//"fmt"
 	"testing"
 
-	//"github.com/certikfoundation/shentu/v2/x/bank"
-	"github.com/stretchr/testify/require"
-
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	bankTypes "github.com/cosmos/cosmos-sdk/x/bank/types"
 
+	"github.com/stretchr/testify/suite"
+	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
+
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	"github.com/cosmos/cosmos-sdk/simapp"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	"github.com/cosmos/cosmos-sdk/x/bank/types"
 )
 
-func TestMsgSendRoute(t *testing.T) {
+type IntegrationTestSuite struct {
+	suite.Suite
+
+	app         *simapp.SimApp
+	ctx         sdk.Context
+	queryClient types.QueryClient
+}
+
+func (suite *IntegrationTestSuite) SetupTest() {
+	app := simapp.Setup(false)
+	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
+
+	app.AccountKeeper.SetParams(ctx, authtypes.DefaultParams())
+	app.BankKeeper.SetParams(ctx, types.DefaultParams())
+
+	queryHelper := baseapp.NewQueryServerTestHelper(ctx, app.InterfaceRegistry())
+	types.RegisterQueryServer(queryHelper, app.BankKeeper)
+	queryClient := types.NewQueryClient(queryHelper)
+
+	suite.app = app
+	suite.ctx = ctx
+	suite.queryClient = queryClient
+}
+
+func TestTypesTestSuite(t *testing.T) {
+	suite.Run(t, new(IntegrationTestSuite))
+}
+
+func (suite *IntegrationTestSuite) TestMsgSendRoute() {
 	addr1 := sdk.AccAddress([]byte("from"))
 	addr2 := sdk.AccAddress([]byte("to"))
 	coins := sdk.NewCoins(sdk.NewInt64Coin("uctk", 10))
 	var msg = NewMsgLockedSend(addr1, addr2, "", coins)
-	require.Equal(t, msg.Route(), bankTypes.RouterKey)
-	require.Equal(t, msg.Type(), "locked_send")
+	suite.Require().Equal(msg.Route(), bankTypes.RouterKey)
+	suite.Require().Equal(msg.Type(), "locked_send")
 }
-
