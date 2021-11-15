49c2f8a093f5db1053e61f9b7f5abf4a16ae0628
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2021-11-15 23:55:53 +0100

    MsgSendValidation unit test on the types bank module

--- x/bank/types/msgs_test.go
+++ x/bank/types/msgs_test.go
@@ -47,8 +47,41 @@ func TestTypesTestSuite(t *testing.T) {
 func (suite *IntegrationTestSuite) TestMsgSendRoute() {
 	addr1 := sdk.AccAddress([]byte("from"))
 	addr2 := sdk.AccAddress([]byte("to"))
+	UnlockerAddress := sdk.AccAddress([]byte("unlocker"))
 	coins := sdk.NewCoins(sdk.NewInt64Coin("uctk", 10))
-	var msg = NewMsgLockedSend(addr1, addr2, "", coins)
+	var msg = NewMsgLockedSend(addr1, addr2, UnlockerAddress.String(), coins)
 	suite.Require().Equal(msg.Route(), bankTypes.RouterKey)
 	suite.Require().Equal(msg.Type(), "locked_send")
+}
+
+
+func (suite *IntegrationTestSuite)  TestMsgSendValidation() {
+	addr1 := sdk.AccAddress([]byte("from"))
+	addr2 := sdk.AccAddress([]byte("to"))
+	UnlockerAddress := sdk.AccAddress([]byte("unlocker"))
+	CTK123 := sdk.NewCoins(sdk.NewInt64Coin("ctk", 123))
+	CTK0 := sdk.NewCoins(sdk.NewInt64Coin("ctk", 0))
+	CTK123eth123 := sdk.NewCoins(sdk.NewInt64Coin("ctk", 123), sdk.NewInt64Coin("eth", 123))
+	CTK123eth0 := sdk.Coins{sdk.NewInt64Coin("ctk", 123), sdk.NewInt64Coin("eth", 0)}
+
+	var emptyAddr sdk.AccAddress
+
+	cases := []struct {
+		tx    *MsgLockedSend
+		valid bool
+	}{
+		{NewMsgLockedSend(addr1, addr2, UnlockerAddress.String(), CTK123), true},       // valid send
+		{NewMsgLockedSend(addr1, addr2, UnlockerAddress.String(), CTK123eth123), true}, // valid send with multiple coins
+		{NewMsgLockedSend(addr1, addr2, UnlockerAddress.String(), CTK0), false},        // non positive coin
+		{NewMsgLockedSend(addr1, addr2, UnlockerAddress.String(), CTK123eth0), false},  // non positive coin in multicoins
+		{NewMsgLockedSend(emptyAddr, addr2, UnlockerAddress.String(), CTK123), false},  // empty from addr
+		{NewMsgLockedSend(addr1, emptyAddr, UnlockerAddress.String(), CTK123), false},  // empty to addr
+	}
+
+	for _, tc := range cases {
+		err := tc.tx.ValidateBasic()
+		if tc.valid {
+			suite.Require().Nil(err)
+		}
+	}
 }
