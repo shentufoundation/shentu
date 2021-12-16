296a7b6d2d28df4b90596d8865b3334c8364e12f
priyanshiiit <priyanshiiit@gmail.com>
2021-12-17 01:37:05 +0530

    Keeper: Tests for deposit operations - refund & delete

--- x/gov/keeper/keeper_test.go
+++ x/gov/keeper/keeper_test.go
@@ -9,7 +9,6 @@ import (
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 
 	"github.com/cosmos/cosmos-sdk/baseapp"
-	"github.com/cosmos/cosmos-sdk/codec"
 	sdksimapp "github.com/cosmos/cosmos-sdk/simapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	govtypes "github.com/cosmos/cosmos-sdk/x/gov/types"
@@ -30,7 +29,6 @@ var (
 type KeeperTestSuite struct {
 	suite.Suite
 
-	cdc         *codec.LegacyAmino
 	app         *simapp.SimApp
 	ctx         sdk.Context
 	keeper      keeper.Keeper
@@ -152,6 +150,118 @@ func (suite *KeeperTestSuite) TestKeeper_ProposeAndDeposit() {
 		}
 	}
 
+}
+
+func (suite *KeeperTestSuite) TestKeeper_DepositOperations() {
+	type proposal struct {
+		title       string
+		description string
+	}
+
+	tests := []struct {
+		name                 string
+		proposal             proposal
+		proposer             sdk.AccAddress
+		fundedCoins          sdk.Coins
+		depositAmount        sdk.Coins
+		finalAmount          sdk.Coins
+		testRefund           bool
+		setInvalidProposalId bool
+		shouldPass           bool
+	}{
+		{
+			name: "Refund all deposits in a specific proposal",
+			proposal: proposal{
+				title:       "title0",
+				description: "description0",
+			},
+			proposer:             suite.address[0],
+			fundedCoins:          sdk.NewCoins(sdk.NewInt64Coin(suite.app.StakingKeeper.BondDenom(suite.ctx), (700)*1e6)),
+			depositAmount:        sdk.NewCoins(sdk.NewInt64Coin(suite.app.StakingKeeper.BondDenom(suite.ctx), (700)*1e6)),
+			finalAmount:          sdk.NewCoins(sdk.NewInt64Coin(suite.app.StakingKeeper.BondDenom(suite.ctx), (700)*1e6)),
+			testRefund:           true,
+			setInvalidProposalId: false,
+			shouldPass:           true,
+		},
+		{
+			name: "Delete all deposits in a specific proposal",
+			proposal: proposal{
+				title:       "title0",
+				description: "description0",
+			},
+			proposer:             suite.address[0],
+			fundedCoins:          sdk.NewCoins(sdk.NewInt64Coin(suite.app.StakingKeeper.BondDenom(suite.ctx), (700)*1e6)),
+			depositAmount:        sdk.NewCoins(sdk.NewInt64Coin(suite.app.StakingKeeper.BondDenom(suite.ctx), (600)*1e6)),
+			finalAmount:          sdk.NewCoins(sdk.NewInt64Coin(suite.app.StakingKeeper.BondDenom(suite.ctx), 100*1e6)),
+			testRefund:           false,
+			setInvalidProposalId: false,
+			shouldPass:           true,
+		},
+		{
+			name: "Refund: Invalid proposal id",
+			proposal: proposal{
+				title:       "title0",
+				description: "description0",
+			},
+			proposer:             suite.address[0],
+			fundedCoins:          sdk.NewCoins(sdk.NewInt64Coin(suite.app.StakingKeeper.BondDenom(suite.ctx), (700)*1e6)),
+			depositAmount:        sdk.NewCoins(sdk.NewInt64Coin(suite.app.StakingKeeper.BondDenom(suite.ctx), (700)*1e6)),
+			testRefund:           true,
+			setInvalidProposalId: true,
+			shouldPass:           false,
+		},
+		{
+			name: "Delete: Invalid proposal id",
+			proposal: proposal{
+				title:       "title0",
+				description: "description0",
+			},
+			proposer:             suite.address[0],
+			fundedCoins:          sdk.NewCoins(sdk.NewInt64Coin(suite.app.StakingKeeper.BondDenom(suite.ctx), (700)*1e6)),
+			depositAmount:        sdk.NewCoins(sdk.NewInt64Coin(suite.app.StakingKeeper.BondDenom(suite.ctx), (700)*1e6)),
+			testRefund:           false,
+			setInvalidProposalId: true,
+			shouldPass:           false,
+		},
+	}
+
+	for _, tc := range tests {
+		textProposalContent := govtypes.NewTextProposal(tc.proposal.title, tc.proposal.description)
+
+		// create/submit a new proposal
+		proposal, err := suite.app.GovKeeper.SubmitProposal(suite.ctx, textProposalContent, tc.proposer)
+		suite.Require().NoError(err)
+
+		// add staking coins to address 1
+		suite.Require().NoError(sdksimapp.FundAccount(suite.app.BankKeeper, suite.ctx, suite.address[1], tc.fundedCoins))
+
+		// deposit staked coins to get the proposal into voting period once it has exceeded minDeposit
+		_, err = suite.app.GovKeeper.AddDeposit(suite.ctx, proposal.ProposalId, suite.address[1], tc.depositAmount)
+		suite.Require().NoError(err)
+		if tc.setInvalidProposalId {
+			proposal.ProposalId = proposal.ProposalId + 10
+		}
+
+		if tc.testRefund {
+			err = suite.app.GovKeeper.RefundDepositsByProposalID(suite.ctx, proposal.ProposalId)
+		} else {
+			err = suite.app.GovKeeper.DeleteDepositsByProposalID(suite.ctx, proposal.ProposalId)
+		}
+
+		if tc.shouldPass {
+			suite.Require().NoError(err)
+			if tc.testRefund {
+				suite.Require().Equal(tc.finalAmount, suite.app.BankKeeper.GetAllBalances(suite.ctx, suite.address[1]))
+			} else {
+				suite.Require().Equal(tc.finalAmount, suite.app.BankKeeper.GetAllBalances(suite.ctx, suite.address[1]))
+			}
+		} else {
+			suite.Require().Error(err)
+		}
+
+		// emptying address 1 for next set of test cases
+		suite.app.BankKeeper.SendCoins(suite.ctx, suite.address[1], suite.address[2], suite.app.BankKeeper.GetAllBalances(suite.ctx, suite.address[1]))
+	}
 }
 
 func (suite *KeeperTestSuite) TestKeeper_Vote() {
