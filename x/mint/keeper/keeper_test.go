cf362dd6c4566b0203fde3ee79c912a3447d91df
fuzious <arpitsrivastava2012@gmail.com>
2022-01-06 02:00:42 +0530

    Refactoring and a Few More Tests

--- x/mint/keeper/keeper_test.go
+++ x/mint/keeper/keeper_test.go
@@ -4,50 +4,85 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/require"
-
-	"github.com/tendermint/tendermint/crypto"
+	"github.com/stretchr/testify/suite"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 
 	sdksimapp "github.com/cosmos/cosmos-sdk/simapp"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
-	minttypes "github.com/cosmos/cosmos-sdk/x/mint/types"
-
 	"github.com/certikfoundation/shentu/v2/simapp"
 )
 
-func TestKeeper_SendToCommunityPool(t *testing.T) {
-	t.Log("Test keeper FundCommunityPool")
-	app := simapp.Setup(false)
-	ctx := app.BaseApp.NewContext(false, tmproto.Header{Time: time.Now().UTC()})
-	coins := sdk.Coins{sdk.NewInt64Coin("uctk", 80000*1e6)}
-	require.NoError(t, sdksimapp.FundModuleAccount(app.BankKeeper, ctx, "mint", coins))
-
-	t.Run("Funding community pool", func(t *testing.T) {
-		moduleAcct := sdk.AccAddress(crypto.AddressHash([]byte("mint")))
-		coins100 := sdk.NewCoins(sdk.NewInt64Coin(app.StakingKeeper.BondDenom(ctx), 100*1e6))
-		bal0 := app.BankKeeper.GetBalance(ctx, moduleAcct, "uctk")
-		err := app.MintKeeper.SendToCommunityPool(ctx, coins100)
-		bal1 := app.BankKeeper.GetBalance(ctx, moduleAcct, "uctk")
-		require.NoError(t, err)
-		require.Equal(t, bal0.Sub(bal1), sdk.NewInt64Coin(app.StakingKeeper.BondDenom(ctx), 100*1e6))
-	})
+// shared setup
+type KeeperTestSuite struct {
+	suite.Suite
+	app *simapp.SimApp
+	ctx sdk.Context
 }
 
-func TestKeeper_SendToShieldRewards(t *testing.T) {
-	t.Log("Test keeper FundShieldRewards")
-	app := simapp.Setup(false)
-	ctx := app.BaseApp.NewContext(false, tmproto.Header{Time: time.Now().UTC()})
+func (suite *KeeperTestSuite) SetupTest() {
+	suite.app = simapp.Setup(false)
+	suite.ctx = suite.app.BaseApp.NewContext(false, tmproto.Header{Time: time.Now().UTC()})
 	coins := sdk.Coins{sdk.NewInt64Coin("uctk", 80000*1e6)}
-	require.NoError(t, sdksimapp.FundModuleAccount(app.BankKeeper, ctx, minttypes.ModuleName, coins))
-	t.Run("Funding Shield Rewards", func(t *testing.T) {
-		moduleAcct := sdk.AccAddress(crypto.AddressHash([]byte("mint")))
-		coins100 := sdk.NewCoins(sdk.NewInt64Coin(app.StakingKeeper.BondDenom(ctx), 100*1e6))
-		bal0 := app.BankKeeper.GetBalance(ctx, moduleAcct, "uctk")
-		err := app.MintKeeper.SendToShieldRewards(ctx, coins100)
-		bal1 := app.BankKeeper.GetBalance(ctx, moduleAcct, "uctk")
-		require.NoError(t, err)
-		require.Equal(t, bal0.Sub(bal1), sdk.NewInt64Coin(app.StakingKeeper.BondDenom(ctx), 100*1e6))
-	})
+	suite.Require().NoError(sdksimapp.FundModuleAccount(suite.app.BankKeeper, suite.ctx, "mint", coins))
+}
+
+func (suite *KeeperTestSuite) TestKeeper_SendToCommunityPool() {
+	tests := []struct {
+		name  string
+		coins sdk.Coins
+		err   bool
+	}{
+		{
+			name:  "Funding Community Pool",
+			coins: sdk.NewCoins(sdk.NewInt64Coin(suite.app.StakingKeeper.BondDenom(suite.ctx), 100*1e6)),
+			err:   false,
+		},
+		{
+			name:  "Funding With 0",
+			coins: sdk.NewCoins(sdk.NewInt64Coin(suite.app.StakingKeeper.BondDenom(suite.ctx), 0)),
+			err:   true,
+		},
+	}
+
+	for _, tc := range tests {
+		suite.T().Log(tc.name)
+		err := suite.app.MintKeeper.SendToCommunityPool(suite.ctx, tc.coins)
+		if tc.err {
+			suite.Require().Nil(err)
+		}
+		suite.Require().NoError(err)
+	}
+}
+
+func (suite *KeeperTestSuite) TestKeeper_SendToShieldRewards() {
+	tests := []struct {
+		name  string
+		coins sdk.Coins
+		err   bool
+	}{
+		{
+			name:  "Funding Shield Rewards",
+			coins: sdk.NewCoins(sdk.NewInt64Coin(suite.app.StakingKeeper.BondDenom(suite.ctx), 100*1e6)),
+			err:   false,
+		},
+		{
+			name:  "Funding With 0",
+			coins: sdk.NewCoins(sdk.NewInt64Coin(suite.app.StakingKeeper.BondDenom(suite.ctx), 0)),
+			err:   true,
+		},
+	}
+
+	for _, tc := range tests {
+		suite.T().Log(tc.name)
+		err := suite.app.MintKeeper.SendToShieldRewards(suite.ctx, tc.coins)
+		if tc.err {
+			suite.Require().Nil(err)
+		}
+		suite.Require().NoError(err)
+	}
+}
+
+func TestKeeperTestSuite(t *testing.T) {
+	suite.Run(t, new(KeeperTestSuite))
 }
