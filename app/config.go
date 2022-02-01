33e1798becac493d278ff21a2fa66cbe45e8bc3b
Arpit Srivastava <arpitsrivastava2012@gmail.com>
2022-02-01 22:16:52 +0530

    Bank: CLI Tests  (#370)
    * bank: cli tests
    
    * bank: lint fix
    
    * bank: keeper tests added
    
    * bank: remove keeper tests
    
    * bank: lint fix
    
    * minor fix
    
    * change uctk to microctkdenom
    
    * run lint
    
    * remove indirect flag registration
    
    Co-authored-by: yoongbok lee <yoongbok.lee@certik.org>
    Co-authored-by: yoongbok-lee <52583590+yoongbok-lee@users.noreply.github.com>

---
+++ app/config.go
@@ -0,0 +1,60 @@
+package app
+
+import (
+	"fmt"
+	"time"
+
+	dbm "github.com/tendermint/tm-db"
+
+	"github.com/cosmos/cosmos-sdk/baseapp"
+	"github.com/cosmos/cosmos-sdk/crypto/hd"
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
+	servertypes "github.com/cosmos/cosmos-sdk/server/types"
+	"github.com/cosmos/cosmos-sdk/simapp"
+	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	"github.com/cosmos/cosmos-sdk/testutil/network"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+
+	"github.com/certikfoundation/shentu/v2/app/params"
+	"github.com/certikfoundation/shentu/v2/common"
+)
+
+// DefaultConfig returns a default configuration suitable for nearly all
+// testing requirements.
+func DefaultConfig() network.Config {
+	encCfg := MakeEncodingConfig()
+
+	return network.Config{
+		Codec:             encCfg.Marshaler,
+		TxConfig:          encCfg.TxConfig,
+		LegacyAmino:       encCfg.Amino,
+		InterfaceRegistry: encCfg.InterfaceRegistry,
+		AccountRetriever:  authtypes.AccountRetriever{},
+		AppConstructor:    NewAppConstructor(encCfg),
+		GenesisState:      ModuleBasics.DefaultGenesis(encCfg.Marshaler),
+		TimeoutCommit:     1 * time.Second,
+		ChainID:           "shentu-code-test",
+		NumValidators:     1,
+		BondDenom:         common.MicroCTKDenom,
+		MinGasPrices:      fmt.Sprintf("0.025%s", common.MicroCTKDenom),
+		AccountTokens:     sdk.TokensFromConsensusPower(1000, sdk.DefaultPowerReduction),
+		StakingTokens:     sdk.TokensFromConsensusPower(500, sdk.DefaultPowerReduction),
+		BondedTokens:      sdk.TokensFromConsensusPower(100, sdk.DefaultPowerReduction),
+		PruningStrategy:   storetypes.PruningOptionNothing,
+		CleanupDir:        true,
+		SigningAlgo:       string(hd.Secp256k1Type),
+		KeyringOptions:    []keyring.Option{},
+	}
+}
+
+func NewAppConstructor(encodingCfg params.EncodingConfig) network.AppConstructor {
+	return func(val network.Validator) servertypes.Application {
+		return NewShentuApp(
+			val.Ctx.Logger, dbm.NewMemDB(), nil, true, make(map[int64]bool), val.Ctx.Config.RootDir, 0,
+			encodingCfg,
+			simapp.EmptyAppOptions{},
+			baseapp.SetMinGasPrices(val.AppConfig.MinGasPrices),
+		)
+	}
+}
