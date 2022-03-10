6db61ef93434a33d71701c8398a82078b5a5955c
yoongbok lee <yoongbok.lee@certik.org>
2022-03-11 02:34:04 +0900

    add store upgrades

--- app/upgrade_handler.go
+++ app/upgrade_handler.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"fmt"
+	crisistypes "github.com/cosmos/cosmos-sdk/x/crisis/types"
 
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -76,14 +77,10 @@ func (app ShentuApp) setShieldV2UpgradeHandler() {
 			// Payout reimbursements
 			v231.PayoutReimbursements(ctx, app.appCodec, app.BankKeeper, app.ShieldKeeper, app.keys[shieldtypes.StoreKey])
 
-			app.IBCKeeper.ConnectionKeeper.SetParams(ctx, ibcconnectiontypes.DefaultParams())
-
 			fromVM := make(map[string]uint64)
-			for moduleName := range app.mm.Modules {
-				fromVM[moduleName] = 2
-			}
 
 			fromVM[shieldtypes.ModuleName] = 1
+			fromVM[crisistypes.ModuleName] = 0
 
 			return app.mm.RunMigrations(ctx, app.configurator, fromVM)
 		},
@@ -95,7 +92,9 @@ func (app ShentuApp) setShieldV2UpgradeHandler() {
 	}
 
 	if upgradeInfo.Name == shieldv2 && !app.UpgradeKeeper.IsSkipHeight(upgradeInfo.Height) {
-		storeUpgrades := storetypes.StoreUpgrades{}
+		storeUpgrades := storetypes.StoreUpgrades{
+			Added: []string{crisistypes.ModuleName},
+		}
 
 		// configure store loader that checks if version == upgradeHeight and applies store upgrades
 		app.SetStoreLoader(upgradetypes.UpgradeStoreLoader(upgradeInfo.Height, &storeUpgrades))
