e876f85bf443382eb50344a4c0034cacc5605276
yoongbok lee <yoongbok.lee@certik.org>
2022-05-26 13:58:52 +0900

    update shield module consensus version

--- x/shield/module.go
+++ x/shield/module.go
@@ -163,7 +163,7 @@ func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.Raw
 }
 
 // ConsensusVersion implements AppModule/ConsensusVersion.
-func (AppModule) ConsensusVersion() uint64 { return 1 }
+func (AppModule) ConsensusVersion() uint64 { return 2 }
 
 // BeginBlock returns the begin blocker for the shield module.
 func (am AppModule) BeginBlock(ctx sdk.Context, rbb abci.RequestBeginBlock) {
