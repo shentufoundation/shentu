c326c7ba6169f656ddff89d03fc8b1b84ca529cd
Neil Ruaro <erickson_ruaroii@dlsu.edu.ph>
2022-06-06 12:59:55 +0800

    removed MixedCoins and MixedDecCoins from proto

--- x/bank/types/tx.pb.go
+++ x/bank/types/tx.pb.go
@@ -530,7 +530,10 @@ func (m *MsgLockedSend) Unmarshal(dAtA []byte) error {
 			if err != nil {
 				return err
 			}
-			if (skippy < 0) || (iNdEx+skippy) < 0 {
+			if skippy < 0 {
+				return ErrInvalidLengthTx
+			}
+			if (iNdEx + skippy) < 0 {
 				return ErrInvalidLengthTx
 			}
 			if (iNdEx + skippy) > l {
@@ -580,7 +583,10 @@ func (m *MsgLockedSendResponse) Unmarshal(dAtA []byte) error {
 			if err != nil {
 				return err
 			}
-			if (skippy < 0) || (iNdEx+skippy) < 0 {
+			if skippy < 0 {
+				return ErrInvalidLengthTx
+			}
+			if (iNdEx + skippy) < 0 {
 				return ErrInvalidLengthTx
 			}
 			if (iNdEx + skippy) > l {
