bdb483a0e593cba39bdc039c2a2e8c5ad9dcc21f
Xiuming Dou <xiuming.dou@certik.com>
2023-01-05 07:37:50 +0000

    recompile .proto files

--- x/bounty/types/genesis.pb.go
+++ x/bounty/types/genesis.pb.go
@@ -5,15 +5,14 @@ package types
 
 import (
 	fmt "fmt"
-	io "io"
-	math "math"
-	math_bits "math/bits"
-
 	_ "github.com/cosmos/cosmos-sdk/codec/types"
 	_ "github.com/cosmos/cosmos-sdk/types"
 	_ "github.com/gogo/protobuf/gogoproto"
 	proto "github.com/gogo/protobuf/proto"
 	_ "github.com/regen-network/cosmos-proto"
+	io "io"
+	math "math"
+	math_bits "math/bits"
 )
 
 // Reference imports to suppress errors if they are not otherwise used.
