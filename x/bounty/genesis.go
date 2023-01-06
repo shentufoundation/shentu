1343d49bcb70aa2bef44e00ee124cafc804f1934
Xiuming Dou <xiuming.dou@certik.com>
2023-01-06 17:08:30 +0000

    git add the bounty genesis.go

---
+++ x/bounty/genesis.go
@@ -0,0 +1,16 @@
+package bounty
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/shentufoundation/shentu/v2/x/bounty/keeper"
+	"github.com/shentufoundation/shentu/v2/x/bounty/types"
+)
+
+// InitGenesis stores genesis parameters.
+func InitGenesis(ctx sdk.Context, k keeper.Keeper, data types.GenesisState) {
+	k.SetNextProgramID(ctx, data.StartingProgramId)
+	// TODO Complete InitGenesis
+}
+
+// TODO Implement ExportGenesis
