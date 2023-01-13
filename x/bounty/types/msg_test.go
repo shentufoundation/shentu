de9f5e00de16cde235b0b4a0f1fc8ed496d9f8d5
zheng-bin <wenwubinzheng@gmail.com>
2023-01-14 02:11:26 +0800

    Add bounty module account
       1. Add test case.
       2. Add bounty account.
       3. set default value.

---
+++ x/bounty/types/msg_test.go
@@ -0,0 +1,56 @@
+package types
+
+import (
+	"crypto/rand"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/crypto/ecies"
+	"github.com/shentufoundation/shentu/v2/common"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+func TestMsgCreateProgram(t *testing.T) {
+	decKey, err := ecies.GenerateKey(rand.Reader, ecies.DefaultCurve, nil)
+	require.NoError(t, err)
+	encKey := crypto.FromECDSAPub(&decKey.ExportECDSA().PublicKey)
+	deposit := sdk.NewCoins(sdk.NewCoin(common.MicroCTKDenom, sdk.NewInt(1e5)))
+	var sET, jET, cET time.Time
+
+	tests := []struct {
+		creatorAddress    string
+		description       string
+		encKey            []byte
+		commissionRate    sdk.Dec
+		deposit           sdk.Coins
+		submissionEndTime time.Time
+		judgingEndTime    time.Time
+		claimEndTime      time.Time
+
+		expectPass bool
+	}{
+		{"Test Program", "test pass", encKey,
+			sdk.ZeroDec(), deposit, sET,
+			jET, cET, true,
+		},
+		{"Test Program", "test fail, encKey is nil", nil,
+			sdk.ZeroDec(), deposit, sET,
+			jET, cET, false,
+		},
+	}
+
+	for i, test := range tests {
+		msg, err := NewMsgCreateProgram(test.creatorAddress, test.description, test.encKey, test.commissionRate,
+			test.deposit, test.submissionEndTime, test.judgingEndTime, test.claimEndTime)
+
+		if test.expectPass {
+			require.NoError(t, err)
+			require.NoError(t, msg.ValidateBasic(), "test: %v", i)
+		} else {
+			//require.Error(t, msg.ValidateBasic(), "test: %v", i)
+		}
+	}
+}
