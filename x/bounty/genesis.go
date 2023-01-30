ab521991d59021377cf235f371b0e2e36139a434
kevin-yuhh <yu-honghai@163.com>
2023-01-30 11:48:24 +0800

    directly use k.AppendFidToFidList to set findingIdList

--- x/bounty/genesis.go
+++ x/bounty/genesis.go
@@ -12,25 +12,13 @@ func InitGenesis(ctx sdk.Context, k keeper.Keeper, data types.GenesisState) {
 	k.SetNextProgramID(ctx, data.StartingProgramId)
 	k.SetNextFindingID(ctx, data.StartingFindingId)
 
-	findingIDMap := make(map[uint64][]uint64)
 	for _, finding := range data.Findings {
 		k.SetFinding(ctx, finding)
-
-		findingList, ok := findingIDMap[finding.ProgramId]
-		if !ok {
-			findingList = []uint64{finding.FindingId}
-		} else {
-			findingList = append(findingList, finding.FindingId)
-		}
-		findingIDMap[finding.ProgramId] = findingList
+		k.AppendFidToFidList(ctx, finding.ProgramId, finding.FindingId)
 	}
 
 	for _, program := range data.Programs {
 		k.SetProgram(ctx, program)
-		findingIDList, ok := findingIDMap[program.ProgramId]
-		if ok {
-			k.SetPidFindingIDList(ctx, program.ProgramId, findingIDList)
-		}
 	}
 }
 
