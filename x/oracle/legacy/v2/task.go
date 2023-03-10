b7e7699bc92a334cbc1db185748c23b313cf2720
zheng-bin <wenwubinzheng@gmail.com>
2023-03-10 18:18:47 +0800

    Change ClosingBlock to ExpireHeight.

--- x/oracle/legacy/v2/task.go
+++ x/oracle/legacy/v2/task.go
@@ -15,23 +15,36 @@ func MigrateTaskStore(ctx sdk.Context, storeKey sdk.StoreKey, cdc codec.BinaryCo
 
 	defer iterator.Close()
 	for ; iterator.Valid(); iterator.Next() {
-		var task types.Task
-		cdc.MustUnmarshalLengthPrefixed(iterator.Value(), &task)
-		bz, err := cdc.MarshalInterface(&task)
+		var oldTask Task
+		cdc.MustUnmarshalLengthPrefixed(iterator.Value(), &oldTask)
+
+		newTask := types.NewTask(
+			oldTask.Contract,
+			oldTask.Function,
+			oldTask.BeginBlock,
+			oldTask.Bounty,
+			oldTask.Description,
+			oldTask.Expiration,
+			sdk.AccAddress(oldTask.Creator),
+			oldTask.ClosingBlock,
+			oldTask.WaitingBlocks,
+		)
+		newTask.Status = types.TaskStatus(oldTask.Status)
+
+		// delete old task
+		oldTaskKey := append(append(types.TaskStoreKeyPrefix, []byte(oldTask.Contract)...), []byte(oldTask.Function)...)
+		store.Delete(oldTaskKey)
+		// set task
+		bz, err := cdc.MarshalInterface(&newTask)
 		if err != nil {
 			panic(err)
 		}
-
-		// delete old task
-		oldTaskKey := append(append(types.TaskStoreKeyPrefix, []byte(task.Contract)...), []byte(task.Function)...)
-		store.Delete(oldTaskKey)
-		// set task
-		store.Set(types.TaskStoreKey(task.GetID()), bz)
+		store.Set(types.TaskStoreKey(newTask.GetID()), bz)
 		// task IDs
-		newTaskID := types.TaskID{Tid: task.GetID()}
+		newTaskID := types.TaskID{Tid: newTask.GetID()}
 		taskIDs = append(taskIDs, newTaskID)
 		bz = cdc.MustMarshalLengthPrefixed(&types.TaskIDs{TaskIds: taskIDs})
-		store.Set(types.ClosingTaskIDsStoreKey(task.ClosingBlock), bz)
+		store.Set(types.ClosingTaskIDsStoreKey(newTask.ExpireHeight), bz)
 	}
 
 	return nil
