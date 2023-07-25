f867ffcb970666a49ce802227d0a81bed42dd970
kevin-yuhh <yu-honghai@163.com>
2023-07-25 10:28:30 +0800

    update ConsensusVersion

--- x/oracle/module.go
+++ x/oracle/module.go
@@ -164,7 +164,7 @@ func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.Raw
 }
 
 // ConsensusVersion implements AppModule/ConsensusVersion.
-func (AppModule) ConsensusVersion() uint64 { return 2 }
+func (AppModule) ConsensusVersion() uint64 { return 3 }
 
 // BeginBlock implements the Cosmos SDK BeginBlock module function.
 func (am AppModule) BeginBlock(ctx sdk.Context, _ abci.RequestBeginBlock) {
