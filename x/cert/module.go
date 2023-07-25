d8df45ea18d34f566f84c1d070827488b7daa06e
kevin-yuhh <yu-honghai@163.com>
2023-07-25 10:29:51 +0800

    update ConsensusVersion

--- x/cert/module.go
+++ x/cert/module.go
@@ -156,7 +156,7 @@ func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.Raw
 }
 
 // ConsensusVersion implements AppModule/ConsensusVersion.
-func (AppModule) ConsensusVersion() uint64 { return 1 }
+func (AppModule) ConsensusVersion() uint64 { return 2 }
 
 //____________________________________________________________________________
 
