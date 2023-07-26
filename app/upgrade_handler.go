a4ec89f86a1658944df9e7654fa1171c5de94a44
Hao Zhang <zhanghao_tt@sina.com>
2023-07-26 10:41:22 +0800

    finish store migration for staking

--- app/upgrade_handler.go
+++ app/upgrade_handler.go
@@ -2,12 +2,12 @@ package app
 
 import (
 	"fmt"
+	"time"
 
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
-	stakingKeeper "github.com/cosmos/cosmos-sdk/x/staking/keeper"
 	stakingTypes  "github.com/cosmos/cosmos-sdk/x/staking/types"
 
 	"github.com/shentufoundation/shentu/v2/common"
@@ -88,7 +88,8 @@ func transAddrPrefixForStaking(ctx sdk.Context, app ShentuApp) (err error) {
 	if err != nil {
 		return err
 	}
-	//transite prefix for redelegation
+	//transite prefix for redelegation and redelegationQueue
+	touchedTimes := make(map[time.Time]bool)
 	skKeeper.IterateRedelegations(ctx, func(idx int64, red stakingTypes.Redelegation) bool {
 		red.DelegatorAddress, err = common.PrefixToShentu(red.DelegatorAddress)
 		if err != nil {
@@ -103,12 +104,37 @@ func transAddrPrefixForStaking(ctx sdk.Context, app ShentuApp) (err error) {
 			return true
 		}
 		skKeeper.SetRedelegation(ctx, red)
+		for _, e := range red.Entries {
+			if touchedTimes[e.CompletionTime] {
+				continue
+			}
+			dvvts := skKeeper.GetRedelegationQueueTimeSlice(ctx, e.CompletionTime)
+			if len(dvvts) == 0 {
+				continue
+			}
+			for _, t := range dvvts {
+				t.DelegatorAddress, err = common.PrefixToShentu(t.DelegatorAddress)
+				if err != nil {
+					return true
+				}
+				t.ValidatorDstAddress, err = common.PrefixToShentu(t.ValidatorDstAddress)
+				if err != nil {
+					return true
+				}
+				t.ValidatorSrcAddress, err = common.PrefixToShentu(t.ValidatorSrcAddress)
+				if err != nil {
+					return true
+				}
+			}
+			skKeeper.SetRedelegationQueueTimeSlice(ctx, e.CompletionTime, dvvts)
+			touchedTimes[e.CompletionTime] = true
+		}
 		return false
 	})
 	if err != nil {
 		return err
 	}
-	//transite prefix for unbonding_delegations
+	//transite prefix for UnbondingDelegation and UnbondingQueue
 	skKeeper.IterateUnbondingDelegations(ctx, func(idx int64, ubd stakingTypes.UnbondingDelegation) bool {
 		ubd.DelegatorAddress, err = common.PrefixToShentu(ubd.DelegatorAddress)
 		if err != nil {
@@ -119,7 +145,41 @@ func transAddrPrefixForStaking(ctx sdk.Context, app ShentuApp) (err error) {
 			return true
 		}
 		skKeeper.SetUnbondingDelegation(ctx, ubd)
+		touchedTimes := make(map[time.Time]bool)
+		for _, e := range ubd.Entries {
+			if touchedTimes[e.CompletionTime] {
+				continue
+			}
+			dvps := skKeeper.GetUBDQueueTimeSlice(ctx, e.CompletionTime)
+			for _, p := range dvps {
+				p.DelegatorAddress, err = common.PrefixToShentu(p.DelegatorAddress)
+				if err != nil {
+					return true
+				}
+				p.ValidatorAddress, err = common.PrefixToShentu(p.ValidatorAddress)
+				if err != nil {
+					return true
+				}
+			}
+			skKeeper.SetUBDQueueTimeSlice(ctx, e.CompletionTime, dvps)
+			touchedTimes[e.CompletionTime] = true
+		}
 		return false
 	})
+	if err != nil {
+		return err
+	}
+	//transite prefix for HistoricalInfo
+	skKeeper.IterateHistoricalInfo(ctx, func(hi stakingTypes.HistoricalInfo) bool {
+		for _, v := range hi.Valset {
+			v.OperatorAddress, err = common.PrefixToShentu(v.OperatorAddress)
+			if err != nil {
+				return true
+			}
+		}
+		skKeeper.SetHistoricalInfo(ctx, hi.Header.Height, &hi)
+		return false
+	})
+
 	return err
 }
