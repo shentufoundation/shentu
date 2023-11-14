c5312f11957a05c6ce3a9a38c89c160271cfa453
zheng-bin <wenwubinzheng@gmail.com>
2023-11-14 17:05:14 +0800

    delete test case

--- common/prefix_test.go
+++ common/prefix_test.go
@@ -2,29 +2,13 @@ package common
 
 import (
 	"crypto/sha256"
-	"github.com/stretchr/testify/require"
 	"strings"
 	"testing"
 
+	"github.com/stretchr/testify/require"
+
 	"github.com/cosmos/cosmos-sdk/types/bech32"
 )
-
-func TestPrefixToCertik(t *testing.T) {
-	sum := sha256.Sum256([]byte("hello world\n"))
-	ss := "shentu"
-
-	address, err := bech32.ConvertAndEncode(ss, sum[:])
-	require.NoError(t, err)
-
-	certikAddr, err := PrefixToCertik(address)
-	require.NoError(t, err)
-	require.True(t, strings.HasPrefix(certikAddr, "certik"))
-
-	address, err = bech32.ConvertAndEncode("certik", sum[:])
-	require.NoError(t, err)
-	_, err = PrefixToCertik(address)
-	require.Error(t, err)
-}
 
 func TestPrefixToShentu(t *testing.T) {
 	sum := sha256.Sum256([]byte("hello world\n"))
@@ -40,5 +24,5 @@ func TestPrefixToShentu(t *testing.T) {
 	address, err = bech32.ConvertAndEncode("shentu", sum[:])
 	require.NoError(t, err)
 	_, err = PrefixToShentu(address)
-	require.Error(t, err)
+	require.NoError(t, err)
 }
