b7691517571022b180043e3ede9d488e2b55cd97
skyargos <binskyargos@gmail.com>
2024-10-17 09:22:25 +0800

    fix gov grpc query

--- x/gov/keeper/grpc_query.go
+++ x/gov/keeper/grpc_query.go
@@ -9,6 +9,7 @@ import (
 	"google.golang.org/grpc/status"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/types/query"
 	govtypesv1 "github.com/cosmos/cosmos-sdk/x/gov/types/v1"
 
 	typesv1 "github.com/shentufoundation/shentu/v2/x/gov/types/v1"
@@ -22,39 +23,180 @@ func NewQueryServer(k *Keeper) typesv1.QueryServer {
 	return queryServer{k: k}
 }
 
-func (q queryServer) Constitution(ctx context.Context, request *govtypesv1.QueryConstitutionRequest) (*govtypesv1.QueryConstitutionResponse, error) {
-	//TODO implement me
-	panic("implement me")
+func (q queryServer) Constitution(ctx context.Context, _ *govtypesv1.QueryConstitutionRequest) (*govtypesv1.QueryConstitutionResponse, error) {
+	constitution, err := q.k.Constitution.Get(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &govtypesv1.QueryConstitutionResponse{Constitution: constitution}, nil
 }
 
-func (q queryServer) Proposal(ctx context.Context, request *govtypesv1.QueryProposalRequest) (*govtypesv1.QueryProposalResponse, error) {
-	//TODO implement me
-	panic("implement me")
+// Proposal returns proposal details based on ProposalID
+func (q queryServer) Proposal(ctx context.Context, req *govtypesv1.QueryProposalRequest) (*govtypesv1.QueryProposalResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	if req.ProposalId == 0 {
+		return nil, status.Error(codes.InvalidArgument, "proposal id can not be 0")
+	}
+
+	proposal, err := q.k.Proposals.Get(ctx, req.ProposalId)
+	if err != nil {
+		if errors.IsOf(err, collections.ErrNotFound) {
+			return nil, status.Errorf(codes.NotFound, "proposal %d doesn't exist", req.ProposalId)
+		}
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &govtypesv1.QueryProposalResponse{Proposal: &proposal}, nil
 }
 
-func (q queryServer) Proposals(ctx context.Context, request *govtypesv1.QueryProposalsRequest) (*govtypesv1.QueryProposalsResponse, error) {
-	//TODO implement me
-	panic("implement me")
+// Proposals implements the Query/Proposals gRPC method
+func (q queryServer) Proposals(ctx context.Context, req *govtypesv1.QueryProposalsRequest) (*govtypesv1.QueryProposalsResponse, error) {
+	filteredProposals, pageRes, err := query.CollectionFilteredPaginate(ctx, q.k.Proposals, req.Pagination, func(key uint64, p govtypesv1.Proposal) (include bool, err error) {
+		matchVoter, matchDepositor, matchStatus := true, true, true
+
+		// match status (if supplied/valid)
+		if govtypesv1.ValidProposalStatus(req.ProposalStatus) {
+			matchStatus = p.Status == req.ProposalStatus
+		}
+
+		// match voter address (if supplied)
+		if len(req.Voter) > 0 {
+			voter, err := q.k.authKeeper.AddressCodec().StringToBytes(req.Voter)
+			if err != nil {
+				return false, err
+			}
+
+			has, err := q.k.Votes.Has(ctx, collections.Join(p.Id, sdk.AccAddress(voter)))
+			// if no error, vote found, matchVoter = true
+			matchVoter = err == nil && has
+		}
+
+		// match depositor (if supplied)
+		if len(req.Depositor) > 0 {
+			depositor, err := q.k.authKeeper.AddressCodec().StringToBytes(req.Depositor)
+			if err != nil {
+				return false, err
+			}
+			has, err := q.k.Deposits.Has(ctx, collections.Join(p.Id, sdk.AccAddress(depositor)))
+			// if no error, deposit found, matchDepositor = true
+			matchDepositor = err == nil && has
+		}
+
+		// if all match, append to results
+		if matchVoter && matchDepositor && matchStatus {
+			return true, nil
+		}
+		// continue to next item, do not include because we're appending results above.
+		return false, nil
+	}, func(_ uint64, value govtypesv1.Proposal) (*govtypesv1.Proposal, error) {
+		return &value, nil
+	})
+
+	if err != nil && !errors.IsOf(err, collections.ErrInvalidIterator) {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &govtypesv1.QueryProposalsResponse{Proposals: filteredProposals, Pagination: pageRes}, nil
 }
 
-func (q queryServer) Vote(ctx context.Context, request *govtypesv1.QueryVoteRequest) (*govtypesv1.QueryVoteResponse, error) {
-	//TODO implement me
-	panic("implement me")
+// Vote returns Voted information based on proposalID, voterAddr
+func (q queryServer) Vote(ctx context.Context, req *govtypesv1.QueryVoteRequest) (*govtypesv1.QueryVoteResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	if req.ProposalId == 0 {
+		return nil, status.Error(codes.InvalidArgument, "proposal id can not be 0")
+	}
+
+	if req.Voter == "" {
+		return nil, status.Error(codes.InvalidArgument, "empty voter address")
+	}
+
+	voter, err := q.k.authKeeper.AddressCodec().StringToBytes(req.Voter)
+	if err != nil {
+		return nil, err
+	}
+	vote, err := q.k.Votes.Get(ctx, collections.Join(req.ProposalId, sdk.AccAddress(voter)))
+	if err != nil {
+		if errors.IsOf(err, collections.ErrNotFound) {
+			return nil, status.Errorf(codes.InvalidArgument,
+				"voter: %v not found for proposal: %v", req.Voter, req.ProposalId)
+		}
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &govtypesv1.QueryVoteResponse{Vote: &vote}, nil
 }
 
-func (q queryServer) Votes(ctx context.Context, request *govtypesv1.QueryVotesRequest) (*govtypesv1.QueryVotesResponse, error) {
-	//TODO implement me
-	panic("implement me")
+// Votes returns single proposal's votes
+func (q queryServer) Votes(ctx context.Context, req *govtypesv1.QueryVotesRequest) (*govtypesv1.QueryVotesResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	if req.ProposalId == 0 {
+		return nil, status.Error(codes.InvalidArgument, "proposal id can not be 0")
+	}
+
+	votes, pageRes, err := query.CollectionPaginate(ctx, q.k.Votes, req.Pagination, func(_ collections.Pair[uint64, sdk.AccAddress], value govtypesv1.Vote) (vote *govtypesv1.Vote, err error) {
+		return &value, nil
+	}, query.WithCollectionPaginationPairPrefix[uint64, sdk.AccAddress](req.ProposalId))
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &govtypesv1.QueryVotesResponse{Votes: votes, Pagination: pageRes}, nil
 }
 
-func (q queryServer) Deposit(ctx context.Context, request *govtypesv1.QueryDepositRequest) (*govtypesv1.QueryDepositResponse, error) {
-	//TODO implement me
-	panic("implement me")
+// Deposit queries single deposit information based on proposalID, depositAddr.
+func (q queryServer) Deposit(ctx context.Context, req *govtypesv1.QueryDepositRequest) (*govtypesv1.QueryDepositResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	if req.ProposalId == 0 {
+		return nil, status.Error(codes.InvalidArgument, "proposal id can not be 0")
+	}
+
+	if req.Depositor == "" {
+		return nil, status.Error(codes.InvalidArgument, "empty depositor address")
+	}
+
+	depositor, err := q.k.authKeeper.AddressCodec().StringToBytes(req.Depositor)
+	if err != nil {
+		return nil, err
+	}
+	deposit, err := q.k.Deposits.Get(ctx, collections.Join(req.ProposalId, sdk.AccAddress(depositor)))
+	if err != nil {
+		return nil, status.Error(codes.NotFound, err.Error())
+	}
+
+	return &govtypesv1.QueryDepositResponse{Deposit: &deposit}, nil
 }
 
-func (q queryServer) Deposits(ctx context.Context, request *govtypesv1.QueryDepositsRequest) (*govtypesv1.QueryDepositsResponse, error) {
-	//TODO implement me
-	panic("implement me")
+// Deposits returns single proposal's all deposits
+func (q queryServer) Deposits(ctx context.Context, req *govtypesv1.QueryDepositsRequest) (*govtypesv1.QueryDepositsResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	if req.ProposalId == 0 {
+		return nil, status.Error(codes.InvalidArgument, "proposal id can not be 0")
+	}
+
+	var deposits []*govtypesv1.Deposit
+	deposits, pageRes, err := query.CollectionPaginate(ctx, q.k.Deposits, req.Pagination, func(_ collections.Pair[uint64, sdk.AccAddress], deposit govtypesv1.Deposit) (*govtypesv1.Deposit, error) {
+		return &deposit, nil
+	}, query.WithCollectionPaginationPairPrefix[uint64, sdk.AccAddress](req.ProposalId))
+	if err != nil {
+		return nil, status.Error(codes.Internal, err.Error())
+	}
+
+	return &govtypesv1.QueryDepositsResponse{Deposits: deposits, Pagination: pageRes}, nil
 }
 
 func (q queryServer) TallyResult(ctx context.Context, req *govtypesv1.QueryTallyResultRequest) (*govtypesv1.QueryTallyResultResponse, error) {
@@ -106,7 +248,7 @@ func (q queryServer) CertVoted(c context.Context, req *typesv1.QueryCertVotedReq
 }
 
 // Params queries all params
-// func (q queryServer) Params(ctx context.Context, request *govtypesv1.QueryParamsRequest) (*typesv1.QueryParamsResponse, error) {
+// func (q queryServer) Params(ctx context.Context, request *govtypesv1.QueryParamsRequest) (*typesgovtypesv1.QueryParamsResponse, error) {
 func (q queryServer) Params(c context.Context, req *govtypesv1.QueryParamsRequest) (*typesv1.QueryParamsResponse, error) {
 	if req == nil {
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
