843ed5127263bd3eea0843a73b5c1b2b9fdf17aa
skyargos <binskyargos@gmail.com>
2025-05-28 11:59:54 +0800

    fix: test-unit-cover err

--- x/bounty/genesis_test.go
+++ x/bounty/genesis_test.go
@@ -4,7 +4,10 @@ import (
 	"reflect"
 	"testing"
 
+	"github.com/cometbft/cometbft/crypto/ed25519"
 	"github.com/stretchr/testify/require"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	shentuapp "github.com/shentufoundation/shentu/v2/app"
 	"github.com/shentufoundation/shentu/v2/x/bounty"
@@ -12,35 +15,49 @@ import (
 )
 
 func TestExportGenesis(t *testing.T) {
+	params := types.DefaultParams()
+	acc1 := sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address())
+
 	dataGS := &types.GenesisState{
 		Programs: []*types.Program{
 			{
-				ProgramId: "1",
+				ProgramId:    "1",
+				Name:         "1",
+				AdminAddress: acc1.String(),
 			},
 			{
-				ProgramId: "2",
+				ProgramId:    "2",
+				Name:         "2",
+				AdminAddress: acc1.String(),
 			},
 			{
-				ProgramId: "3",
+				ProgramId:    "3",
+				Name:         "3",
+				AdminAddress: acc1.String(),
 			},
 		},
 		Findings: []*types.Finding{
 			{
-				FindingId: "1",
-				ProgramId: "1",
+				FindingId:        "1",
+				ProgramId:        "1",
+				SubmitterAddress: acc1.String(),
 			},
 			{
-				FindingId: "4",
-				ProgramId: "3",
+				FindingId:        "4",
+				ProgramId:        "3",
+				SubmitterAddress: acc1.String(),
 			},
 		},
+		StartingTheoremId: 1,
+		Params:            &params,
 	}
 
 	app1 := shentuapp.Setup(t, false)
 	ctx1 := app1.BaseApp.NewContext(false)
 	k1 := app1.BountyKeeper
 
-	bounty.InitGenesis(ctx1, app1.AccountKeeper, k1, dataGS)
+	err := bounty.InitGenesis(ctx1, app1.AccountKeeper, k1, dataGS)
+	require.NoError(t, err)
 	exported1 := bounty.ExportGenesis(ctx1, k1)
 
 	app2 := shentuapp.Setup(t, false)
@@ -50,7 +67,8 @@ func TestExportGenesis(t *testing.T) {
 	exported2 := bounty.ExportGenesis(ctx2, k2)
 	require.False(t, reflect.DeepEqual(exported1, exported2))
 
-	bounty.InitGenesis(ctx2, app2.AccountKeeper, k2, exported1)
+	err = bounty.InitGenesis(ctx2, app2.AccountKeeper, k2, exported1)
+	require.NoError(t, err)
 	exported3 := bounty.ExportGenesis(ctx2, k2)
 	require.True(t, reflect.DeepEqual(exported1, exported3))
 }
