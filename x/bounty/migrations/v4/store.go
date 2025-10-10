543e5229f717252b0a1cb5a8d45fdff35a0e85f4
skyargos <binskyargos@gmail.com>
2025-10-10 17:55:24 +0800

    fix(bounty) migration bounty params

--- x/bounty/migrations/v4/store.go
+++ x/bounty/migrations/v4/store.go
@@ -1,35 +1,58 @@
 package v4
 
 import (
+	"fmt"
+
 	"cosmossdk.io/collections"
-	"cosmossdk.io/core/store"
+	corestoretypes "cosmossdk.io/core/store"
 	sdkmath "cosmossdk.io/math"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
+	"github.com/shentufoundation/shentu/v2/common"
 	"github.com/shentufoundation/shentu/v2/x/bounty/types"
 )
 
-func MigrateStore(ctx sdk.Context, storeService store.KVStoreService, cdc codec.BinaryCodec) error {
-	sb := collections.NewSchemaBuilder(storeService)
+// MigrateStore migrates the bounty module state from version 4 to version 5.
+// It reads params from raw KVStore to handle protobuf schema changes.
+// Protobuf automatically handles removed fields (CheckerRate) and new fields get default values.
+func MigrateStore(ctx sdk.Context, storeService corestoretypes.KVStoreService, cdc codec.BinaryCodec) error {
+	store := storeService.OpenKVStore(ctx)
 
-	params := collections.NewItem(sb, types.ParamsKey, "params", codec.CollValue[types.Params](cdc))
-	oldParam, err := params.Get(ctx)
+	// Get the params key bytes
+	paramsKey := collections.NewPrefix(61)
+
+	// Read existing params from raw store
+	// Protobuf will automatically ignore the old CheckerRate field
+	paramsBz, err := store.Get(paramsKey)
 	if err != nil {
-		return err
+		return fmt.Errorf("failed to get params from store: %w", err)
 	}
+
+	ctx.Logger().Info("migrating bounty params", "data_length", len(paramsBz))
+
+	var oldParam Params
+	err = cdc.Unmarshal(paramsBz, &oldParam)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal old params (len=%d): %w", len(paramsBz), err)
+	}
+
+	// Create new params with the new fields set
+	// ComplexityFee and MaxComplexity are new fields added in this version
 	newParam := types.Params{
 		MinGrant:              oldParam.MinGrant,
 		MinDeposit:            oldParam.MinDeposit,
 		TheoremMaxProofPeriod: oldParam.TheoremMaxProofPeriod,
 		ProofMaxLockPeriod:    oldParam.ProofMaxLockPeriod,
-		ComplexityFee:         sdk.NewCoin("uctk", sdkmath.NewInt(10000)),
+		ComplexityFee:         sdk.NewCoin(common.MicroCTKDenom, sdkmath.NewInt(10000)),
 		MaxComplexity:         int64(1000000),
 	}
-	err = params.Set(ctx, newParam)
+
+	// Marshal and save updated params
+	bz, err := cdc.Marshal(&newParam)
 	if err != nil {
 		return err
 	}
 
-	return nil
+	return store.Set(paramsKey, bz)
 }
